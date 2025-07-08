186266711449b6253db10f0a7d3a0bf5ab5772e2
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-08 19:21:09 +0200

    feat(domain): implement Hand class

---
+++ src/main/kotlin/blackjack/Hand.kt
@@ -0,0 +1,13 @@
+package blackjack
+
+class Hand(private val hold: Hold) {
+    constructor() : this(Hold(setOf()))
+
+    fun numberOfCards(): Int {
+        return hold.cards.size
+    }
+
+    fun addCard(card: Card) {
+        this.hold.add(card)
+    }
+}
