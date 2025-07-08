3ac9a9cabf0bc8d2273f2b5a2071a96426e4460c
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-08 16:54:49 +0200

    test(domain): implement a mew test for CardDeck class.

---
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -0,0 +1,12 @@
+package blackjack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CardDeckTest {
+    @Test
+    fun `CardDeck has 52 cards`() {
+        val cardDeck = CardDeck()
+        assertThat(cardDeck.cards.hold).hasSize(52)
+    }
+}
