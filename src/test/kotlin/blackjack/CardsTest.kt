e2413ac4368ffa4909b758cc1855a8d24cc45533
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-08 17:44:22 +0200

    test(domain): add test for cardDeck shuffling

--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -21,4 +21,28 @@ class CardsTest {
 
         assertThat(hand.hold.last()).isEqualTo(card)
     }
+
+    @Test
+    fun `should be able to move a card and remove that card from the cardDesk`() {
+        val cards =
+            Cards(
+                setOf(
+                    Card(Symbol.HEART, 1),
+                    Card(Symbol.HEART, 2),
+                    Card(Symbol.HEART, 3),
+                    Card(Symbol.HEART, 4),
+                ),
+            )
+        val hand = Cards()
+        cards.move(hand)
+        assertThat(cards.hold).hasSize(3)
+    }
+
+    @Test
+    fun `order of cards in two cardDesks is different`() {
+        val cardDesk1 = CardDeck().cards.hold.toList()
+        val cardDesk2 = CardDeck().cards.hold.toList()
+
+        assertThat(cardDesk1).isNotEqualTo(cardDesk2)
+    }
 }
