39e5f7cebe20471cbbc2a3858767702ce30f1012
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-08 14:48:14 +0200

    test: add a new test to implement Card class

---
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -0,0 +1,13 @@
+package blackjack
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CardTest {
+    @Test
+    fun `throw if card is illegal`() {
+        assertThrows<IllegalArgumentException> {
+            Card()
+        }
+    }
+}
