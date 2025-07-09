ea5e214fe4022208dd259c24a57eba7972e1ed3f
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-09 11:59:17 +0200

    add(Deck): functionality of generating and drawing a card/s

---
+++ src/main/kotlin/blackjack/Deck.kt
@@ -0,0 +1,35 @@
+package blackjack
+
+
+
+
+class Deck {
+    var cards: MutableList<Card> = mutableListOf()
+        private set
+
+    init {
+        generateCards()
+        cards.shuffle()
+        println(cards)
+    }
+
+    fun drawCard(count: Int = 1): List<Card> {
+        val drawnCards = cards.take(count)
+        removeCards(drawnCards)
+        return drawnCards
+    }
+
+    private fun removeCards(element: List<Card>) {
+        element.forEach { card ->
+            cards.remove(card)
+        }
+    }
+
+    private fun generateCards() {
+        cards = Suit.entries.map { suit ->
+            Rank.entries.map { rank ->
+                Card(rank, suit)
+            }
+        }.flatten().toMutableList()
+    }
+}
