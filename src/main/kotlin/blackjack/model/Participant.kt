48da8c33ce64a66447d1ba0158995901c4f9cc52
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-09 19:31:38 +0200

    feat(controller): implement game logic to determine outcomes
    Add methods to handle card dealing for players and dealer. Implement logic to calculate game outcomes, determining wins, losses, and ties based on scores.

--- src/main/kotlin/blackjack/model/Participant.kt
+++ src/main/kotlin/blackjack/model/Participant.kt
@@ -2,16 +2,25 @@ package blackjack.model
 
 abstract class Participant(val name: String) {
     protected val hand = Hand()
+    val gameResults: GameResults = GameResults()
 
     init {
         require(name.isNotBlank()) { "Wrong name: $name. Participant name should not be blank." }
     }
 
-    fun isBlackjack(): Boolean = hand.isBlackjack()
+    fun hasBlackJack(): Boolean = hand.hasBlackJack()
 
     fun isBusts(): Boolean = hand.isBusts()
 
     fun addCard(newCard: Card) = hand.addCard(newCard)
+
+    fun getScore(): Int = hand.getScore()
+
+    fun setWin() = gameResults.wins++
+
+    fun setLose() = gameResults.loses++
+
+    fun setTie() = gameResults.ties++
 
     override fun toString(): String {
         return "$name's cards: $hand"
