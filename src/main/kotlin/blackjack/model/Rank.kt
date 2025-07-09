f667387bb6b1a56aee5882ff38e72e2bcfa51843
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-09 16:32:06 +0200

    refactor(Rank): change value of Ace to 11

--- src/main/kotlin/blackjack/model/Rank.kt
+++ src/main/kotlin/blackjack/model/Rank.kt
@@ -1,7 +1,6 @@
 package blackjack.model
 
 enum class Rank(val value: Int) {
-    ACE(1),
     TWO(2),
     THREE(3),
     FOUR(4),
@@ -14,4 +13,5 @@ enum class Rank(val value: Int) {
     JACK(10),
     QUEEN(10),
     KING(10),
+    ACE(11),
 }
