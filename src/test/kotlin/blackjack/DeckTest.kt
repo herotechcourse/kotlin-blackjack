69e52bb7fd54144bdc001472ce1eedb5c50fbe9d
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-09 13:53:39 +0200

    chore: remove unnecessary import

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -2,35 +2,33 @@ package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertEquals
 
 class DeckTest {
     @Test
-    fun `returns 52 - size of the original deck`(){
+    fun `returns 52 - size of the original deck`() {
         val deck = Deck()
         assertThat(deck.cards.size).isEqualTo(52)
     }
 
     @Test
-    fun `returns 51 - size of the original deck after removeCards(1)`(){
+    fun `returns 51 - size of the original deck after removeCards(1)`() {
         val deck = Deck()
         deck.drawCard()
         assertThat(deck.cards.size).isEqualTo(51)
     }
 
     @Test
-    fun `returns 50 - size of the original deck after removeCards(2)`(){
+    fun `returns 50 - size of the original deck after removeCards(2)`() {
         val deck = Deck()
         deck.drawCard(2)
         assertThat(deck.cards.size).isEqualTo(50)
     }
 
     @Test
-    fun `returns false - Card removed from the List`(){
+    fun `returns false - Card removed from the List`() {
         val deck = Deck()
         val card = deck.drawCard().first()
 
         assertThat(deck.cards.contains(card)).isFalse()
     }
-
 }
