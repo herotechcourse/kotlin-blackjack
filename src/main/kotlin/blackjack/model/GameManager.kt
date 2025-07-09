63df228d8f3ef70823aefdad545becc0f146402e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-09 18:54:07 +0200

    feat: get single player result and play with multiple players

--- src/main/kotlin/blackjack/model/GameManager.kt
+++ src/main/kotlin/blackjack/model/GameManager.kt
@@ -7,8 +7,45 @@ class GameManager(private val dealer: Player, private val players: List<Player>)
         setUp()
     }
 
-    private fun setUp() {
+    fun setUp() {
         players.forEach { cardDeck.hit(it, 2) }
         cardDeck.hit(dealer)
+    }
+
+    fun finalResult(
+        players: List<Player>,
+        goToLoop: () -> Boolean,
+    ): List<PlayerResult> {
+        return players.map {
+            if (it === dealer) {
+                singlePlayerResult(it, { true })
+            } else {
+                singlePlayerResult(it, goToLoop)
+            }
+        }.toList()
+    }
+
+    fun singlePlayerResult(
+        player: Player,
+        goToLoop: () -> Boolean,
+    ): PlayerResult {
+        while (goToLoop() && ableToReceive(player)) {
+            cardDeck.hit(player)
+//            val currentResult = PlayerResult(player)
+//            println("${currentResult.name} ${ currentResult.cards } - ${currentResult.points}")
+            // OutputView.printPlayerResult(PlayerResult(player))
+        }
+        return PlayerResult(player)
+    }
+
+    private fun ableToReceive(player: Player): Boolean {
+        val isDealer = player === dealer
+        if (isDealer) return dealer.calculatePoints() <= ABLE_TO_RECEIVE
+        return player.calculatePoints() <= BLACKJACK
+    }
+
+    companion object {
+        private const val ABLE_TO_RECEIVE = 16
+        private const val BLACKJACK = 21
     }
 }
