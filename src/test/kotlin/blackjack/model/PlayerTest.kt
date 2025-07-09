ddfa273dd0a31d87976a3c310e9a231bd6c0a949
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-09 16:33:52 +0200

    test(PlayerTest): check right sum of cards with aces

--- src/test/kotlin/blackjack/model/PlayerTest.kt
+++ src/test/kotlin/blackjack/model/PlayerTest.kt
@@ -15,6 +15,18 @@ class PlayerTest {
     }
 
     @Test
+    fun `should calculate right sum of values when contain ACEs in cards`() {
+        val card1 = Card(Rank.ACE, Suit.HEARTS)
+        val card2 = Card(Rank.ACE, Suit.DIAMONDS)
+        val card3 = Card(Rank.FIVE, Suit.DIAMONDS)
+        player.drawCard(card1)
+        player.drawCard(card2)
+        player.drawCard(card3)
+        player.calculateTotalValueOfCards()
+        assertThat(player.calculateTotalValueOfCards()).isEqualTo(17)
+    }
+
+    @Test
     fun `should calculate sum of values of cards in hand`() {
         val card1 = Card(Rank.TWO, Suit.HEARTS)
         val card2 = Card(Rank.THREE, Suit.HEARTS)
