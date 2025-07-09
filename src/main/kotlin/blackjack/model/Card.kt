1a06d6716f7e5b5c11bf62b41618db1d5d0fcee7
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-09 13:23:08 +0200

    feat(Card): add method to change visibility

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -1,7 +1,11 @@
 package blackjack.model
 
-class Card(val rank: Rank, val suit: Suit) {
+class Card(val rank: Rank, val suit: Suit, var visibility: Boolean = false) {
     override fun toString(): String {
         return "${rank.name} of ${suit.name}"
+    }
+
+    fun changeVisibilty() {
+        visibility = true
     }
 }
