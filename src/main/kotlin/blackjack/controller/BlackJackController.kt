62bca5ab929fdece5f8ce30252338a199b50fbf3
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-09 11:44:43 +0200

    feat(domain): add game controller and basic logic

---
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -0,0 +1,19 @@
+package blackjack.controller
+
+import blackjack.model.PlayerFactory
+
+object BlackJackController {
+    fun play() {
+        // show prompt
+        // ask/read user input: players name
+        val names = listOf("mina", "vito")
+
+        // create players, create dealer -> store
+        PlayerFactory.with(names)
+
+        // give it to manager
+        // hit the cards
+        // show table cards
+        // asking y or n for new card
+    }
+}
