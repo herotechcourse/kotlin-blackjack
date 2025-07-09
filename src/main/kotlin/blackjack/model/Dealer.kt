4dfeca9706eceeeb0703e4cb94ba666c66e3a896
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-09 17:12:47 +0200

    feat(model): add override toString logic in Dealer

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -1,13 +1,20 @@
 package blackjack.model
 
-class Dealer(name: String): Participant(name) {
+class Dealer(name: String = "Dealer") : Participant(name) {
     val deck = Deck()
+    var showAll = false
 
     fun dealCard(): Card {
         return deck.drawCard()
     }
 
     fun shouldStand(): Boolean = hand.getScore() > DEALER_STAND
+
+    override fun toString(): String = when {
+        showAll -> "$name's cards: $hand"
+        hand.dealtCards.isEmpty() -> "$name has no cards yet."
+        else -> "$name: ${hand.dealtCards[0]}"
+    }
 
     companion object {
         const val DEALER_STAND = 16
