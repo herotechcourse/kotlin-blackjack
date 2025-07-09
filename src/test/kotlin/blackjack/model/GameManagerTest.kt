7a5459fcd7cf05468a67dba577fab3194bbbed71
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-09 14:24:01 +0200

    test: add a new test to set up the table with GameManager

---
+++ src/test/kotlin/blackjack/model/GameManagerTest.kt
@@ -0,0 +1,18 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GameManagerTest {
+    @Test
+    fun `init game manager, the manage should set up a game`() {
+        val listOfName = listOf("Vito", "Mina")
+        val players = PlayerFactory.with(listOfName)
+        val dealer = PlayerFactory.createDealer()
+
+        val gameManager = GameManager(dealer, players)
+        assertThat(dealer.numberInHand()).isEqualTo(1)
+        assertThat(players[0].numberInHand()).isEqualTo(2)
+        assertThat(players[1].numberInHand()).isEqualTo(2)
+    }
+}
