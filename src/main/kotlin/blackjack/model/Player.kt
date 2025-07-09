e352bd07cc926c3d8d34555ced8f78be154cf6b9
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-09 17:21:00 +0200

    refactor(Player): use methods from interface

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -1,28 +1,7 @@
 package blackjack.model
 
-class Player(override val name: String, override var isActive: Boolean = true) :
-    Participant {
-    override val cardsInHand: MutableList<Card> = mutableListOf()
-
-    override fun drawCard(card: Card) {
-        cardsInHand += card
-    }
-
-    private fun checkAces() = cardsInHand.count { card -> card.rank == Rank.ACE }
-
-    override fun calculateTotalValueOfCards(): Int {
-        var totalValueOfCards = cardsInHand.sumOf { card -> card.rank.value }
-        var aceCounter = checkAces()
-        while (totalValueOfCards > 21 && aceCounter > 0) {
-            totalValueOfCards -= 10
-            aceCounter--
-        }
-        return totalValueOfCards
-    }
-
-    override fun updateActiveStatus(totalValueOfCards: Int) {
-        if (totalValueOfCards > 21) {
-            isActive = false
-        }
-    }
-}
+class Player(
+    override val name: String,
+    override var isActive: Boolean = true,
+    override val cardsInHand: MutableList<Card> = mutableListOf(),
+) : Participant
