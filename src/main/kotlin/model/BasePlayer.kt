da8f410381394019ff880b66fc220f9e02d44a44
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-09 17:18:29 +0200

    feat(player) add check if name is empty

--- src/main/kotlin/model/BasePlayer.kt
+++ src/main/kotlin/model/BasePlayer.kt
@@ -3,8 +3,16 @@ package model
 abstract class BasePlayer(val name: String) {
     private val hand = Hand()
 
+    init {
+        require(name.isNotEmpty() && name.isNotBlank()) { "Name must not be empty" }
+    }
+
     fun getScore(): Int {
         return hand.scoreOnHand()
+    }
+
+    fun getCardsNumber(): Int {
+        return hand.getCards().size
     }
 
     abstract fun makeDecision(): Boolean
