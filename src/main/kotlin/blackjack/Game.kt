ae5e4a8b52b688bb63ba29d27133fab86698356b
saragrosser <sgrosser00@gmail.com>
2025-07-09 15:26:47 +0200

    feat(game): allow each player to draw additional cards

--- src/main/kotlin/blackjack/Game.kt
+++ src/main/kotlin/blackjack/Game.kt
@@ -5,5 +5,20 @@ class Game {
         val playerNames = InputView.askPlayerNames()
         val players = playerNames.map { Player(it) }
         val dealer = Dealer()
+
+        OutputView.displayDealing(playerNames)
+
+        repeat(2) {
+            dealer.giveCardTo(dealer)
+            players.forEach { player ->
+                dealer.giveCardTo(player)
+            }
+        }
+        OutputView.displayDealerFirstTwoCards(dealer.getHand().take(2))
+        players.forEach { OutputView.displayPlayerHand(it) }
+
+        println()
+
+        players.forEach { dealer.askPlayerDraw(it) }
     }
 }
