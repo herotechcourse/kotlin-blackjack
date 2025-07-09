8a3ed939e8a3cb00b8f4d04460c4e0ff1200705b
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-09 17:09:10 +0200

    feat(input): add getPlayersNames and getAnswer

---
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -0,0 +1,25 @@
+package blackjack.view
+
+object InputView {
+    fun getPlayersNames(): List<String> {
+        println("Enter the names of the players (coma-separated):")
+        val input = readLine() ?: ""
+        val playersNames = input.trim().split(",").map { it.trim() }
+        require(playersNames.isNotEmpty()) {
+            "Names cannot be empty. You need to provide minimum one name."
+        }
+        require(playersNames.toSet().size == playersNames.size) {
+            "Names should be unique."
+        }
+        return playersNames
+    }
+
+    fun getAnswer(): String {
+        println("Would you like to draw another card? (y for yes, n for no)")
+        val input = readLine()?.trim()?.lowercase() ?: ""
+        require(input == "y" || input == "n") {
+            "Please answer in order to proceed with the game."
+        }
+        return input
+    }
+}
