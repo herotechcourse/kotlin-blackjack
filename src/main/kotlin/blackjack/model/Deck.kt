0a2f45d3d3ac230da963510b7f1000001e61ce8a
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-09 12:26:12 +0200

    feat(Deck): create a set of cards

---
+++ src/main/kotlin/blackjack/model/Deck.kt
@@ -0,0 +1,21 @@
+package blackjack.model
+
+class Deck {
+    private val cards: MutableList<Card> =
+        Suit
+            .entries
+            .flatMap { suit ->
+                Rank.entries.map { rank -> Card(rank, suit) }
+            }.toMutableList()
+
+    fun getCards(): List<Card> {
+        return cards
+    }
+
+    fun shuffleCards() = cards.shuffled()
+
+    fun draw(): Card {
+        require(cards.isNotEmpty()) { "Deck is empty!" }
+        return cards.removeAt(0)
+    }
+}
