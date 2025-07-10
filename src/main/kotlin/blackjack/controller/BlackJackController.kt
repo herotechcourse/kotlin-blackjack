14f0563c442772b1f2b7bb099bcda75c970a86a0
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 18:58:28 +0200

    style: remove comments

--- src/main/kotlin/blackjack/controller/BlackJackController.kt
+++ src/main/kotlin/blackjack/controller/BlackJackController.kt
@@ -8,19 +8,12 @@ import blackjack.view.OutputView
 object BlackJackController {
     fun play() {
         try {
-            // show prompt
-            // ask/read user input: players name
             val names = InputView.getPlayersName()
-
-            // create players, create dealer -> store
             val players = PlayerFactory.with(names)
             val dealer = PlayerFactory.createDealer()
 
-            val totalPlayer = players + dealer
-            // give it to manager && set up
             val gameManager = GameManager(dealer, players)
             gameManager.setUp()
-
             OutputView.printAllPlayers(listOf(dealer) + players)
 
             val totalPlayResult = gameManager.playGame(totalPlayer) { InputView.askForCard() }
