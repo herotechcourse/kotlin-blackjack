6172bdeb2cfa04a55e7a4e7443843e36aa28c034
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-10 16:44:09 +0200

    feat(OutputView): implement method to display results

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,15 +1,45 @@
 package blackjack.view
 
+import blackjack.model.Dealer
 import blackjack.model.Participant
+import blackjack.model.Player
+import kotlin.collections.joinToString
 
 class OutputView {
+    fun displayInitialCards(participants: List<Participant>) {
+        val names = participants.joinToString(", ") { it.name }
+        println("Dealing two cards to $names.")
+        participants.forEach {
+            if (it is Dealer) {
+                println("Dealer: ${it.cardsInHand[0].rank.face}${it.cardsInHand[0].suit.symbol}")
+            }
+            displayHandCards(it)
+        }
+    }
+
     fun displayHandCards(participant: Participant) {
-        println(
-            "${participant.name}'s cards: ${
-                participant
-                    .cardsInHand
-                    .joinToString(", ") { card -> (card.rank.face + card.suit.symbol) }
-            }",
-        )
+        println(participant.storePlayerHand())
+    }
+
+    fun displayDealerDrawMessage(mustDraw: Boolean) {
+        println("Dealer draws one more card due to having 16 or less.")
+    }
+
+    fun displayCardsWithTotalValue(participants: List<Participant>) {
+        participants.forEach {
+            println("${it.storePlayerHand()} - Total: ${it.calculateTotalValueOfCards()}")
+        }
+    }
+
+    fun displayFinalResults(
+        dealer: Dealer,
+        players: List<Player>,
+    ) {
+        println("## Final Results")
+        println(dealer.getFinalResultForDealer(players))
+        players.forEach {
+            print("${it.name}: ")
+            if (it.isActive) println("Win") else println("Lose")
+        }
     }
 }
