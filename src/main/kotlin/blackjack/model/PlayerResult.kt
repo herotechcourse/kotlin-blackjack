05a1fd1ed16dd98ba7385c44b284e75705b1c416
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 16:28:06 +0200

    refactor: delete playerResult class

--- src/main/kotlin/blackjack/model/PlayerResult.kt
+++
@@ -1,7 +0,0 @@
-package blackjack.model
-
-data class PlayerResult(private val player: Player) {
-    val name = player.name
-    val cards = player.cards
-    val points = player.calculatePoints()
-}
