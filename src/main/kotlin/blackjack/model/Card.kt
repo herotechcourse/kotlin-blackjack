78a7956d90af7bb5f13e9d64dd444f8885df88d3
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 16:24:54 +0200

    refactor: override toString

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -1,7 +1,7 @@
 package blackjack.model
 
-data class Card(val suit: Suit, val rank: Rank)
-
-fun Card.toText(): String {
-    return "${this.rank.face}${this.suit.symbol}"
+data class Card(val suit: Suit, val rank: Rank) {
+    override fun toString(): String {
+        return "${rank.face}${suit.symbol}"
+    }
 }
