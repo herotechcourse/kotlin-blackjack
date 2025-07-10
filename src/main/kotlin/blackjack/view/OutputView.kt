3d42fa8741f252975c795e2bfd53677d93f666cc
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 18:38:58 +0200

    feat: print final result statistics

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,6 +1,7 @@
 package blackjack.view
 
 import blackjack.model.Player
+import blackjack.model.Statistics
 
 object OutputView {
     fun printAllPlayers(players: List<Player>) {
@@ -8,7 +9,7 @@ object OutputView {
     }
 
     fun printOnePlayer(player: Player) {
-        println("${player.name}'s cards: " + "${player.cards}")
+        println("${player.name}'s cards: " + player.cardsToString())
     }
 
     fun printDealerDrawsCards(player: Player) {
@@ -17,5 +18,29 @@ object OutputView {
 
     fun printAskForCard(player: Player) {
         println("Would ${player.name} like to draw another card? (y for yes, n for no)")
+    }
+
+    fun printOnePlayerFinalResult(player: Player) {
+        println("${player.name}'s cards: " + "${player.cardsToString()} - Total: ${player.calculatePoints()}.")
+    }
+
+    fun printFinalResults(players: List<Player>) {
+        players.forEach { printOnePlayerFinalResult(it) }
+    }
+
+    private fun printWinOrLose(condition: Boolean): String {
+        return when (condition) {
+            true -> "Win"
+            false -> "Lose"
+        }
+    }
+
+    fun printStatistics(statistics: Statistics) {
+        println("\n## Final Results")
+        println("Dealer: ${statistics.dealerWin} Win ${statistics.dealerLose} Lose")
+        statistics.totalResult.forEach {
+                (player, winnings) ->
+            println("${player.name}: ${printWinOrLose(winnings == 1)}")
+        }
     }
 }
