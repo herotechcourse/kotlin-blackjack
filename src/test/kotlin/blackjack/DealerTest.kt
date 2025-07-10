0c1d521e20af45d98ed0d1d0f5bcfb75966e201e
saragrosser <sgrosser00@gmail.com>
2025-07-10 17:29:14 +0200

    Test(dealer) add test for specific dealer rule >16 or <17

--- src/test/kotlin/blackjack/DealerTest.kt
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -24,5 +24,22 @@ class DealerTest {
         val newSize = player.getHand().size
         assertEquals(initialSize + 1, newSize)
     }
+    @Test
+    fun `should draw when dealer score is 16 or less`() {
+        dealer.addCard(Card(Suit.HEART, Rank.NINE))
+        dealer.addCard(Card(Suit.CLUB, Rank.SEVEN))
 
+        val shouldDraw = dealer.shouldDraw()
+
+        assertTrue(shouldDraw)
+    }
+    @Test
+    fun `should not draw when dealer score is 17 or more`() {
+        dealer.addCard(Card(Suit.SPADE, Rank.TEN))
+        dealer.addCard(Card(Suit.DIAMOND, Rank.SEVEN))
+
+        val shouldDraw = dealer.shouldDraw()
+
+        assertFalse(shouldDraw)
+    }
 }
