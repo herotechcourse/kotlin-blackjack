727c1c1ff2c27f147e99c97c9b3f32ad69008ed0
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 17:23:42 +0200

    refactor: Output View

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -3,13 +3,19 @@ package blackjack.view
 import blackjack.model.Player
 
 object OutputView {
-    fun printFinalResult(players: List<Player>) {
-        players.map {
-            println("\"${it.name}'s cards: \" + \"${it.cards} - Total: ${it.calculatePoints()}")
-        }
+    fun printAllPlayers(players: List<Player>) {
+        players.forEach { printOnePlayer(it) }
     }
 
-    fun printSetUp(players: List<Player>) {
-        players.forEach { println("${it.name}'s cards: " + "${it.cards}") }
+    fun printOnePlayer(player: Player) {
+        println("${player.name}'s cards: " + "${player.cards}")
+    }
+
+    fun printDealerDrawsCards(player: Player) {
+        println("Dealer draws ${player.numberInHand() - 1} more card due to having 16 or less.")
+    }
+
+    fun printAskForCard(player: Player) {
+        println("Would ${player.name} like to draw another card? (y for yes, n for no)")
     }
 }
