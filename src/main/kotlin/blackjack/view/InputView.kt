d532e9dd93f188e7b68069eccaf1c104053864e4
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-10 15:21:18 +0200

    feat: add input validation for players names

---
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -0,0 +1,11 @@
+package blackjack.view
+
+object InputView {
+    fun getPlayersName(): List<String> {
+        println("Enter the names of the players (comma-separated):")
+        val input = (readlnOrNull() ?: throw IllegalArgumentException(Errors.INVALID_INPUT.message)).trim()
+        return input.split(",").also {
+            require(input.isNotEmpty()) { Errors.INVALID_PLAYERS_NAMES.message }
+        }
+    }
+}
