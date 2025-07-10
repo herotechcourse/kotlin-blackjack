1d45233c94867219018e3fbe50ba78821601cf61
Sanghun Park <po4865@gmail.com>
2025-07-10 15:32:17 +0200

    refactor(Stats): add constants to improve readability of game result
    - use companion object to assign constant values for win/lose/tie

--- src/main/kotlin/blackjack/model/Stats.kt
+++ src/main/kotlin/blackjack/model/Stats.kt
@@ -1,17 +1,14 @@
 package blackjack.model
 
 data class Stats(val players: List<Player>, val dealer: Dealer) {
-    // playerBoard["player"] => 0 -> Lose, 1 -> Win, 2 -> Tie
     private var _playerBoard = initPlayerBoard()
     val playerBoard: Map<Player, Int> get() = _playerBoard
 
-    // [0] -> Lose, [1] -> Win, [2] -> Tie
     private var _dealerStats = mapOf("win" to 0, "lose" to 0, "tie" to 0)
     val dealerStats: Map<String, Int> get() = _dealerStats
 
     val dealerScore: Int get() = dealer.calculateHand()
 
-    // create and init 'playerBoard'
     private fun initPlayerBoard(): Map<Player, Int> {
         val board = mutableMapOf<Player, Int>()
         players.forEach { recordPlayerBoard(it, board) }
@@ -24,22 +21,28 @@ data class Stats(val players: List<Player>, val dealer: Dealer) {
     ) {
         val playerScore = player.calculateHand()
         when {
-            player.isBust() -> board[player] = 0
-            dealer.isBust() -> board[player] = 1
-            playerScore > dealerScore -> board[player] = 1
-            playerScore < dealerScore -> board[player] = 0
-            else -> board[player] = 2
+            player.isBust() -> board[player] = LOSE
+            dealer.isBust() -> board[player] = WIN
+            playerScore > dealerScore -> board[player] = WIN
+            playerScore < dealerScore -> board[player] = LOSE
+            else -> board[player] = TIE
         }
     }
 
     fun updateDealerStats() {
         val stats = _dealerStats.toMutableMap()
-        val winCount = playerBoard.values.count { it == 0 }
-        val loseCount = playerBoard.values.count { it == 1 }
-        val tieCount = playerBoard.values.count { it == 2 }
+        val winCount = playerBoard.values.count { it == LOSE }
+        val loseCount = playerBoard.values.count { it == WIN }
+        val tieCount = playerBoard.values.count { it == TIE }
         stats["win"] = winCount
         stats["lose"] = loseCount
         stats["tie"] = tieCount
         _dealerStats = stats.toMap()
+    }
+
+    companion object {
+        const val LOSE = 0
+        const val WIN = 1
+        const val TIE = 2
     }
 }
