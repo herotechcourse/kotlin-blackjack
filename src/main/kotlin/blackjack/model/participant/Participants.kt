b5b607b54fd7eb754aa471f0417009374e63aabe
Mina Kim <mina.kim@deliveryhero.com>
2025-07-11 18:46:20 +0200

    feat: implement Participants class
    - class initiated with two constructors: with name of players or list of players
    - should track class constructor usage

--- src/main/kotlin/blackjack/model/participant/Participants.kt
+++ src/main/kotlin/blackjack/model/participant/Participants.kt
@@ -6,15 +6,17 @@ class Participants private constructor(private val players: List<Participant>) :
     constructor(vararg player: Player) : this(player.toList())
     constructor(vararg names: String) : this(names.map { Player(it) })
 
-    internal fun containAll(name: String): Boolean {
+    internal fun contain(name: String): Boolean {
         return players.any { it.name == name }
     }
 
-    internal fun containAll(vararg names: String): Boolean {
-        return names.all { containAll(it)}
+    internal fun containsAll(vararg names: String): Boolean {
+        return players.map { it.name }.containsAll(names.toList())
     }
 
-    fun containAll(participant: Participant): Boolean {
+    operator fun get(index: Int) = players[index]
+
+    fun contain(participant: Participant): Boolean {
         return participant::class == dealer::class || players.any { it == participant }
     }
 }
