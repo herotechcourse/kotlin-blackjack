806863b1ca2af46fdadca81920d54c5deeeb59f8
Mina Kim <mina.kim@deliveryhero.com>
2025-07-11 15:52:56 +0200

    test: create new Test for Participant/Player/Dealer, and Participants

---
+++ src/test/kotlin/blackjack/model/ParticipantTest.kt
@@ -0,0 +1,24 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ParticipantTest {
+    @Test
+    fun `Participants create players and dealer, and recognize by name`() {
+        val name = "mina"
+        val participants = Participants(name)
+
+        assertThat(participants.contain(name)).isTrue()
+    }
+
+    @Test
+
+    fun `Participants create players and dealer, and recognize by type of Participant`() {
+        val player = Player("name")
+        val participants = Participants(player)
+        val dealer = Dealer()
+
+        assertThat(participants.contain(dealer)).isTrue()
+    }
+}
