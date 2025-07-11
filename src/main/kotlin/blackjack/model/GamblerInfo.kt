f92bff13984177d88dbab922e2b6797645bef5ed
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-11 14:30:35 +0200

    refactor(GamblerInfo): Remove TODO and add error message

--- src/main/kotlin/blackjack/model/GamblerInfo.kt
+++ src/main/kotlin/blackjack/model/GamblerInfo.kt
@@ -3,6 +3,10 @@ package blackjack.model
 @JvmInline
 value class GamblerInfo(val name: String) {
     init {
-        require(name.isNotBlank()) {} // TODO: add err msg
+        require(name.isNotBlank()) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "Name cannot be blank"
     }
 }
