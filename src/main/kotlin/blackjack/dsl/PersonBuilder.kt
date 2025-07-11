1ffa6d8137e99705e6cbc8db729d39e5c781a370
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-11 19:33:46 +0200

    feat: implement dsl languages block

--- src/main/kotlin/blackjack/dsl/PersonBuilder.kt
+++ src/main/kotlin/blackjack/dsl/PersonBuilder.kt
@@ -1,8 +1,10 @@
 package blackjack.dsl
 
-data class Person(val name: String, val company: String?, val skills: Skills)
+data class Person(val name: String, val company: String?, val skills: Skills, val languages: Languages)
 
 data class Skills(val soft: MutableList<String>, val hard: MutableList<String>)
+
+data class Languages(val languages: MutableMap<String, Int>)
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
     return PersonBuilder().apply(block).build()
@@ -12,6 +14,7 @@ class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
     private lateinit var skills: Skills
+    private lateinit var languages: Languages
 
     fun name(value: String) {
         name = value
@@ -25,7 +28,11 @@ class PersonBuilder {
         skills = SkillsBuilder().apply(block).build()
     }
 
-    fun build(): Person = Person(name, company, skills)
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        languages = LanguagesBuilder().apply(block).build()
+    }
+
+    fun build(): Person = Person(name, company, skills, languages)
 }
 
 class SkillsBuilder() {
@@ -41,4 +48,14 @@ class SkillsBuilder() {
     }
 
     fun build(): Skills = Skills(softSkills, hardSkills)
+}
+
+class LanguagesBuilder() {
+    private var languages: MutableMap<String, Int> = mutableMapOf()
+
+    infix fun String.level(level: Int) {
+        languages[this] = level
+    }
+
+    fun build(): Languages = Languages(languages)
 }
