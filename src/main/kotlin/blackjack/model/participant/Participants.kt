232202860b6c57e830fc727a8c49382dfe793cba
Mina Kim <mina.kim@deliveryhero.com>
2025-07-11 16:06:39 +0200

    feat: implement containAll method for test in Participants

--- src/main/kotlin/blackjack/model/participant/Participants.kt
+++ src/main/kotlin/blackjack/model/participant/Participants.kt
@@ -6,11 +6,15 @@ class Participants private constructor(private val players: List<Participant>) :
     constructor(vararg player: Player) : this(player.toList())
     constructor(vararg names: String) : this(names.map { Player(it) })
 
-    internal fun contain(name: String): Boolean {
+    internal fun containAll(name: String): Boolean {
         return players.any { it.name == name }
     }
 
-    fun contain(participant: Participant): Boolean {
+    internal fun containAll(vararg names: String): Boolean {
+        return names.all { containAll(it)}
+    }
+
+    fun containAll(participant: Participant): Boolean {
         return participant::class == dealer::class || players.any { it == participant }
     }
 }
