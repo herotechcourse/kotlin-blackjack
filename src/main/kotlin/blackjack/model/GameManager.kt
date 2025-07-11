fc49b0d1d568cf2fda75a478faa920b45c63e666
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-11 13:52:09 +0200

    refactor: add setUp carDeck in init instead of calling the function from the controller

--- src/main/kotlin/blackjack/model/GameManager.kt
+++ src/main/kotlin/blackjack/model/GameManager.kt
@@ -5,7 +5,7 @@ import blackjack.view.OutputView
 class GameManager(private val dealer: Player, private val players: List<Player>) {
     private val cardDeck = CardDeck()
 
-    fun setUp() {
+    init {
         players.forEach { cardDeck.hit(it, 2) }
         cardDeck.hit(dealer)
     }
