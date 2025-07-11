cc5b45506b4964f7e4e083af5d62901f6550b370
Mina Kim <mina.kim@deliveryhero.com>
2025-07-11 21:54:24 +0200

    refactor: remove useless files at current process

--- src/main/kotlin/blackjack/model/PlayerFactory.kt
+++
@@ -1,9 +0,0 @@
-package blackjack.model
-
-object PlayerFactory {
-    fun createPlayers(names: List<String>) = names.map { PlayerBackup(it) }
-
-    fun createDealer() = PlayerBackup(DEALER)
-
-    const val DEALER = "Dealer"
-}
