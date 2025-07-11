90a1cb5a34d9c46bd570bd1974cd1719bcb6809c
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-11 14:53:41 +0200

    refactor(Deck): Convert same type of test to Parameterized Test

--- src/test/kotlin/blackjack/GamblerInfoTest.kt
+++ src/test/kotlin/blackjack/GamblerInfoTest.kt
@@ -4,19 +4,15 @@ import blackjack.model.GamblerInfo
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class GamblerInfoTest {
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = ["", "    "])
     fun `should throw exception - empty input`() {
         assertThrows<IllegalArgumentException> {
             GamblerInfo("")
-        }
-    }
-
-    @Test
-    fun `should throw exception - blank input`() {
-        assertThrows<IllegalArgumentException> {
-            GamblerInfo("    ")
         }
     }
 
