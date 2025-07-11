b76465ec2a153df10636f9afa47d18aa4d8d2fc3
Mina Kim <mina.kim@deliveryhero.com>
2025-07-11 21:55:07 +0200

    test: remove not usable test

--- src/test/kotlin/blackjack/model/HandTest.kt
+++ src/test/kotlin/blackjack/model/HandTest.kt
@@ -3,61 +3,47 @@ package blackjack.model
 import blackjack.model.card.Card
 import blackjack.model.card.Rank
 import blackjack.model.card.Suit
-import org.assertj.core.api.Assertions.assertThat
+import blackjack.model.holder.Hand
 import org.junit.jupiter.api.Test
 
 class HandTest {
     @Test
-    fun calculatePoints() {
-        val player = PlayerBackup("doggo")
-        val listOfCards =
-            listOf(
+    fun `init Hand`() {
+        Hand()
+    }
+
+    companion object {
+        object DoesNotHasAce {
+            val TOTAL_SUM_16 = listOf(
+                Card(Suit.DIAMONDS, Rank.TWO),
+                Card(Suit.DIAMONDS, Rank.QUEEN),
+                Card(Suit.DIAMONDS, Rank.FOUR),
+            )
+        }
+
+        object HasAce {
+            val TOTAL_SUM_17 = listOf(
                 Card(Suit.DIAMONDS, Rank.ACE),
                 Card(Suit.DIAMONDS, Rank.TWO),
                 Card(Suit.DIAMONDS, Rank.QUEEN),
                 Card(Suit.DIAMONDS, Rank.FOUR),
             )
-        listOfCards.forEach { player.addCard(it) }
-        assertThat(player.calculatePoints()).isEqualTo(17)
-    }
+        }
 
-    @Test
-    fun hasPoints2() {
-        val player = PlayerBackup("doggo")
-        val listOfCards =
-            listOf(
-                Card(Suit.DIAMONDS, Rank.ACE),
-                Card(Suit.DIAMONDS, Rank.JACK),
-            )
-        listOfCards.forEach { player.addCard(it) }
-        assertThat(player.calculatePoints()).isEqualTo(21)
-    }
-
-    @Test
-    fun hasPoints3() {
-        val player = PlayerBackup("doggo")
-        val listOfCards =
-            listOf(
-                Card(Suit.DIAMONDS, Rank.ACE),
-                Card(Suit.HEART, Rank.ACE),
-                Card(Suit.SPADES, Rank.ACE),
-                Card(Suit.DIAMONDS, Rank.JACK),
-            )
-        listOfCards.forEach { player.addCard(it) }
-        assertThat(player.calculatePoints()).isEqualTo(13)
-    }
-
-    @Test
-    fun hasPoints4() {
-        val player = PlayerBackup("doggo")
-        val listOfCards =
-            listOf(
+        object HasTripleAce {
+            val TOTAL_SUM_20 = listOf(
                 Card(Suit.DIAMONDS, Rank.ACE),
                 Card(Suit.HEART, Rank.ACE),
                 Card(Suit.SPADES, Rank.ACE),
                 Card(Suit.DIAMONDS, Rank.SEVEN),
             )
-        listOfCards.forEach { player.addCard(it) }
-        assertThat(player.calculatePoints()).isEqualTo(20)
+
+            val TOTAL_SUM_13 = listOf(
+                Card(Suit.DIAMONDS, Rank.ACE),
+                Card(Suit.HEART, Rank.ACE),
+                Card(Suit.SPADES, Rank.ACE),
+                Card(Suit.DIAMONDS, Rank.JACK),
+            )
+        }
     }
 }
