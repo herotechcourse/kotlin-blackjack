7d88efeeffa96804feb9ad649e61bf1cffde4a86
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-07-12 16:09:12 +0200

    refactor(model): simplify dealer result logic by using `Result.inverse`

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -11,34 +11,12 @@ class Dealer(name: String = "Dealer", private val deck: Deck = Deck.generateADec
     fun shouldNotStand(): Boolean = hand.getScore() <= DEALER_STAND
     fun showAllCards() { showAllCards = true }
 
-    fun setResultFor(player: Player) {
-        val result = evaluateResult(player)
-        player.recordResult(result)
-        recordResult(inverse(result))
-    }
-
     override fun toString(): String =
         when {
             showAllCards -> "$name's cards: $hand"
             hand.cards.isEmpty() -> "$name has no cards yet."
             else -> "$name: ${hand.cards[0]}"
         }
-
-    private fun evaluateResult(player: Player): Result = when {
-            player.isBusts() -> Result.LOSE
-            this.isBusts() -> Result.WIN
-            player.hasBlackJack() && !this.hasBlackJack() -> Result.WIN
-            player.getScore() > this.getScore() -> Result.WIN
-            player.getScore() < this.getScore() -> Result.LOSE
-            else -> Result.TIE
-        }
-
-    private fun inverse(result: Result): Result = when (result) {
-        Result.WIN -> Result.LOSE
-        Result.LOSE -> Result.WIN
-        Result.TIE -> Result.TIE
-    }
-
 
     companion object {
         const val DEALER_STAND = 16
--- src/main/kotlin/blackjack/model/Result.kt
+++ src/main/kotlin/blackjack/model/Result.kt
@@ -4,4 +4,11 @@ enum class Result(val description: String) {
     WIN("Win"),
     LOSE("Lose"),
     TIE("Tie");
+
+    val inverse: Result
+        get() = when (this) {
+            WIN -> LOSE
+            LOSE -> WIN
+            TIE -> TIE
+        }
 }
