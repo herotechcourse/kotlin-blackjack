2cfccd133b1c0eb2c5409a60a4945b4d33e3dff0
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-12 17:58:50 +0200

    refactor(Card): remove equals and hash for data class

--- src/main/kotlin/model/Card.kt
+++ src/main/kotlin/model/Card.kt
@@ -1,13 +1,3 @@
 package model
 
-data class Card(val rank: Rank, val suite: Suite, val faceUp: Boolean = true) {
-    override fun equals(other: Any?): Boolean {
-        return rank == (other as Card).rank && suite == other.suite
-    }
-
-    override fun hashCode(): Int {
-        var result = rank.hashCode()
-        result = 52 * result + suite.hashCode()
-        return result
-    }
-}
+data class Card(val rank: Rank, val suite: Suite, val faceUp: Boolean = true)
