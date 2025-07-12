aa7fda14ce3aa1dff24950f68200b7db6bcb4532
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 23:57:24 +0200

    test: add new test for InputView

---
+++ src/test/kotlin/blackjack/view/InputViewTest.kt
@@ -0,0 +1,11 @@
+package blackjack.view
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class InputViewTest {
+    @Test
+    fun `name should be parsed correctly`() {
+
+    }
+}
