a14a2d997c78471e9a7dfa7f17c615142e7e7bb6
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 01:59:23 +0200

    refactor: delete a test in Player due to logic removed

--- src/test/kotlin/blackjack/model/participant/PlayerTest.kt
+++ src/test/kotlin/blackjack/model/participant/PlayerTest.kt
@@ -8,11 +8,4 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
 
-    @ValueSource(strings = ["#####", "%23!", "m1112@", "111133/22"])
-    @ParameterizedTest
-    fun `player should have the correct name`(name: String) {
-        assertThrows<IllegalArgumentException> {
-            Player(name)
-        }
-    }
 }
