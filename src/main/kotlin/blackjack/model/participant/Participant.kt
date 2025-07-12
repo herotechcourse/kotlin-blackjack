51f9e799a346ae766f2bec863f741d55dd1c438c
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 00:51:01 +0200

    refactor: took the logic used in the first PR and refactored it

--- src/main/kotlin/blackjack/model/participant/Participant.kt
+++ src/main/kotlin/blackjack/model/participant/Participant.kt
@@ -1,5 +1,22 @@
 package blackjack.model.participant
 
+import blackjack.model.card.Rank
 import blackjack.model.holder.Hand
 
-abstract class Participant(val name: String) : Hand()
+abstract class Participant(val name: String) : Hand() {
+    val points = getPoints()
+
+    private fun getPoints(): Int {
+        return when (cards.isEmpty()) {
+            true -> 0
+            false -> calculatePoints()
+        }
+    }
+
+    private fun calculatePoints(): Int {
+        val aceCount = cards.filter { it.rank.value == Rank.ACE.value }.size
+        var sum = cards.sumOf { it.rank.value }
+        repeat(aceCount) { if (sum <= 11) sum += 10 }
+        return sum
+    }
+}
