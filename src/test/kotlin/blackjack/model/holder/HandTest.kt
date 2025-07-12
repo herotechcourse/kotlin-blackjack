a7c72d0f819a360f8f17ec696455b26f35752829
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 21:08:00 +0200

    test: add new test for an edge case that Hand try to `draw()` when List is empty

--- src/test/kotlin/blackjack/model/holder/HandTest.kt
+++ src/test/kotlin/blackjack/model/holder/HandTest.kt
@@ -1,10 +1,19 @@
 package blackjack.model.holder
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class HandTest {
     @Test
     fun `init Hand`() {
         Hand()
+    }
+
+    @Test
+    fun `throw if hand is empty, but try to draw`() {
+        val emptyHand = Hand()
+        assertThrows<IllegalStateException> {
+            emptyHand.drawCard()
+        }
     }
 }
