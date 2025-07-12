08fa6ae6e1313b3bd1c07f19f1c4b5a3ee2ee10c
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 22:33:16 +0200

    test: add a test for Deck. check Deck is created by initDeck() method

--- src/test/kotlin/blackjack/model/holder/DeckTest.kt
+++ src/test/kotlin/blackjack/model/holder/DeckTest.kt
@@ -1,13 +1,21 @@
 package blackjack.model.holder
 
+import blackjack.model.holder.Deck.Companion.initDeck
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class DeckTest {
     @Test
+    fun `init deck return sorted 52 cards`() {
+        val cards = initDeck()
+        assertThat(cards).hasSize(Deck.FULL_DECK_SIZE)
+
+    }
+
+    @Test
     fun `empty card deck generate new deck`() {
         val deck = Deck()
-        val oldDeck = deck.cards
+        val oldDeck = deck.cards.toList()
 
         repeat(Deck.FULL_DECK_SIZE) { deck.draw() }
         assertThat(deck.cards).isNotEqualTo(oldDeck)
