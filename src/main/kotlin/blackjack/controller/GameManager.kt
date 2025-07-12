1380732ffc796ac2680f4c659a0241ea2d876060
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 22:33:39 +0200

    refactor: apply new Deck in GameManager

--- src/main/kotlin/blackjack/controller/GameManager.kt
+++ src/main/kotlin/blackjack/controller/GameManager.kt
@@ -1,12 +1,12 @@
 package blackjack.controller
 
-import blackjack.model.holder.CardDeck
+import blackjack.model.holder.Deck
 import blackjack.model.participant.Participant
 
 class GameManager(
     private val participant: Participant,
 ) {
-    private val cardDeck = CardDeck()
+    private val cardDeck = Deck()
 
     companion object {
         private const val ABLE_TO_RECEIVE = 16
