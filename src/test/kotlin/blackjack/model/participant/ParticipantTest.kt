31253be320f8d9c3273f1e0ae453c31dee280f78
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 23:17:41 +0200

    test: create ParticipantTest to test common logic between Participant subclass

--- src/test/kotlin/blackjack/model/participant/ParticipantTest.kt
+++ src/test/kotlin/blackjack/model/participant/ParticipantTest.kt
@@ -1,41 +1,19 @@
 package blackjack.model.participant
 
-import org.assertj.core.api.Assertions
+import blackjack.model.holder.Deck
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ParticipantTest {
-    @Test
-    fun `Participants init with player and dealer, and recognize by name`() {
-        val names = listOf("mina", "guri", "life", "hard")
-        val participants = Participants.from(names)
-
-        Assertions.assertThat(names.all { participants.contain(it) }).isTrue
-    }
 
     @Test
-    fun `Participants init with player and dealer, and recognize by type of Participant`() {
-        val names = listOf("mina", "guri", "life", "hard")
-        val participants = Participants.from(names)
-        val dealer = participants.getDealer()
+    fun `Participant sub classes execute card exchange logic with Deck`() {
+        val deck = Deck()
+        val player = Player("mina")
+        val card = deck.draw()
 
-        Assertions.assertThat(participants.contain(dealer)).isTrue()
-    }
-
-    @Test
-    fun `Participants init with players`() {
-        val names = listOf("mina", "guri")
-        val participants = Participants.from(names)
-
-        Assertions.assertThat(participants.containsAll("mina", "guri")).isTrue()
-        Assertions.assertThat(participants.containsAll("guri", "I am not Guri")).isFalse()
-        Assertions.assertThat(participants.getPlayer(0).name).isEqualTo("mina")
-    }
-
-    @Test
-    fun `can recognize number of players`() {
-        val names = listOf("mina", "guri")
-        val participants = Participants.from(names)
-
-        Assertions.assertThat(participants.getPlayers().size).isEqualTo(2)
+        player.receive(card)
+        assertThat(player.peekLast()).isEqualTo(card)
+        assertThat(player.peekLast()).isSameAs(card)
     }
 }
