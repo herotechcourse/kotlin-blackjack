6c3f1833d542378bb58ea45f0fe432e4e414defa
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 00:57:09 +0200

    revert: get lines from step 1 first PR in GameManager

--- src/main/kotlin/blackjack/controller/GameManager.kt
+++ src/main/kotlin/blackjack/controller/GameManager.kt
@@ -8,6 +8,58 @@ class GameManager(
 ) {
     private val cardDeck = Deck()
 
+    fun setUp() {
+        players.forEach { cardDeck.hit(it, 2) }
+        cardDeck.hit(dealer)
+    }
+
+    fun playGame(
+        dealer: Player,
+        players: List<Player>,
+        askForCard: () -> Boolean = { true },
+    ) {
+        players.forEach { round(it, askForCard) }
+        round(dealer)
+    }
+
+    internal fun round(
+        player: Player,
+        askForCard: () -> Boolean = { true },
+    ) {
+        when (player) {
+            dealer -> roundForDealer(player)
+            else -> roundForPlayers(player, askForCard)
+        }
+    }
+
+    private fun roundForPlayers(
+        player: Player,
+        askForCard: () -> Boolean,
+    ) {
+        while (ableToReceive(player)) {
+            OutputView.printAskForCard(player)
+            if (askForCard()) {
+                cardDeck.hit(player)
+                OutputView.printOnePlayer(player)
+            } else {
+                break
+            }
+        }
+    }
+
+    private fun roundForDealer(player: Player) {
+        while (ableToReceive(player)) {
+            cardDeck.hit(player)
+        }
+        OutputView.printDealerDrawsCards(player)
+    }
+
+    private fun ableToReceive(player: Player): Boolean {
+        val isDealer = player === dealer
+        if (isDealer) return dealer.calculatePoints() <= ABLE_TO_RECEIVE
+        return player.calculatePoints() < BLACKJACK
+    }
+
     companion object {
         private const val ABLE_TO_RECEIVE = 16
         private const val BLACKJACK = 21
