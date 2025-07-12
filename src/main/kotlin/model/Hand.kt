db591b61ad82a0b3627dd02c844a70839a9298cd
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-12 18:30:31 +0200

    refactor: refactor logic to get score from object to function

--- src/main/kotlin/model/Hand.kt
+++ src/main/kotlin/model/Hand.kt
@@ -13,21 +13,20 @@ class Hand {
         return getScore(cards)
     }
 
-    companion object {
-        fun getScore(cards: Set<Card>): Int {
-            var scoreTotal = scoreSum(cards)
-            var aceCounter = aceCount(cards)
-            while (scoreTotal > 21 && hasAce(aceCounter)) {
-                scoreTotal -= 10
-                aceCounter--
-            }
-            return scoreTotal
+    fun getScore(cards: Set<Card>): Int {
+        var scoreTotal = scoreSum(cards)
+        var aceCounter = aceCount(cards)
+        while (scoreTotal > 21 && hasAce(aceCounter)) {
+            scoreTotal -= 10
+            aceCounter--
         }
-
-        private fun hasAce(aceCounter: Int): Boolean = aceCounter != 0
-
-        private fun scoreSum(cards: Set<Card>) = cards.sumOf { card -> Rank.score(card.rank) }
-
-        private fun aceCount(cards: Set<Card>) = cards.count { it.rank == Rank.ACE }
+        return scoreTotal
     }
+
+    private fun hasAce(aceCounter: Int): Boolean = aceCounter != 0
+
+    private fun scoreSum(cards: Set<Card>) = cards.sumOf { card -> Rank.score(card.rank) }
+
+    private fun aceCount(cards: Set<Card>) = cards.count { it.rank == Rank.ACE }
 }
+
