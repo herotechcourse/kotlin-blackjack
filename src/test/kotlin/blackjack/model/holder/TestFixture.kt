b586a1dc2d23ac04596740d43f828edc3de68630
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 22:18:24 +0200

    test: refactor TestFixture class to object

--- src/test/kotlin/blackjack/model/holder/TestFixture.kt
+++ src/test/kotlin/blackjack/model/holder/TestFixture.kt
@@ -4,40 +4,43 @@ import blackjack.model.card.Card
 import blackjack.model.card.Rank
 import blackjack.model.card.Suit
 
-class TestFixture {
+object TestFixture {
 
-    companion object {
-        object DoesNotHasAce {
-            val TOTAL_SUM_16 = listOf(
-                Card(Suit.DIAMONDS, Rank.TWO),
-                Card(Suit.DIAMONDS, Rank.QUEEN),
-                Card(Suit.DIAMONDS, Rank.FOUR),
-            )
-        }
+    object DoesNotHasAce {
+        val TOTAL_SUM_16 = listOf(
+            Card(Suit.DIAMONDS, Rank.TWO),
+            Card(Suit.DIAMONDS, Rank.QUEEN),
+            Card(Suit.DIAMONDS, Rank.FOUR),
+        )
+    }
 
-        object HasAce {
-            val TOTAL_SUM_17 = listOf(
-                Card(Suit.DIAMONDS, Rank.ACE),
-                Card(Suit.DIAMONDS, Rank.TWO),
-                Card(Suit.DIAMONDS, Rank.QUEEN),
-                Card(Suit.DIAMONDS, Rank.FOUR),
-            )
-        }
+    object HasAce {
+        val TOTAL_SUM_17 = listOf(
+            Card(Suit.DIAMONDS, Rank.ACE),
+            Card(Suit.DIAMONDS, Rank.TWO),
+            Card(Suit.DIAMONDS, Rank.QUEEN),
+            Card(Suit.DIAMONDS, Rank.FOUR),
+        )
+    }
 
-        object HasTripleAce {
-            val TOTAL_SUM_20 = listOf(
-                Card(Suit.DIAMONDS, Rank.ACE),
-                Card(Suit.HEART, Rank.ACE),
-                Card(Suit.SPADES, Rank.ACE),
-                Card(Suit.DIAMONDS, Rank.SEVEN),
-            )
+    object HasTripleAce {
+        val TOTAL_SUM_20 = listOf(
+            Card(Suit.DIAMONDS, Rank.ACE),
+            Card(Suit.HEART, Rank.ACE),
+            Card(Suit.SPADES, Rank.ACE),
+            Card(Suit.DIAMONDS, Rank.SEVEN),
+        )
 
-            val TOTAL_SUM_13 = listOf(
-                Card(Suit.DIAMONDS, Rank.ACE),
-                Card(Suit.HEART, Rank.ACE),
-                Card(Suit.SPADES, Rank.ACE),
-                Card(Suit.DIAMONDS, Rank.JACK),
-            )
-        }
+        val TOTAL_SUM_13 = listOf(
+            Card(Suit.DIAMONDS, Rank.ACE),
+            Card(Suit.HEART, Rank.ACE),
+            Card(Suit.SPADES, Rank.ACE),
+            Card(Suit.DIAMONDS, Rank.JACK),
+        )
+    }
+
+    object Card {
+        val DIAMOND_JACK = Card(Suit.DIAMONDS, Rank.JACK)
+        val HEART_ACE = Card(Suit.HEART, Rank.ACE)
     }
 }
