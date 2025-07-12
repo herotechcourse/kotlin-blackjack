073244a3ec2ea73b5f6b4f7b064f111dd4f3c4fe
Mina Kim <mina.kim@deliveryhero.com>
2025-07-12 22:19:03 +0200

    test: add new tests for CardHolder core logics.

--- src/test/kotlin/blackjack/model/holder/HandTest.kt
+++ src/test/kotlin/blackjack/model/holder/HandTest.kt
@@ -1,5 +1,6 @@
 package blackjack.model.holder
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -13,7 +14,27 @@ class HandTest {
     fun `throw if hand is empty, but try to draw`() {
         val emptyHand = Hand()
         assertThrows<IllegalStateException> {
-            emptyHand.drawCard()
+            emptyHand.draw()
         }
+    }
+
+    @Test
+    fun `CardHolder_Hand receive card`() {
+        val hand = Hand()
+        val card = TestFixture.Card.HEART_ACE
+
+        hand.receive(card)
+       assertEquals(card, hand.peek())
+    }
+
+    @Test
+    fun `CardHolder_Hand draw card`() {
+        val hand = Hand()
+        val card = TestFixture.Card.HEART_ACE
+
+        hand.receive(card)
+        val drawCard = hand.draw()
+        assertEquals(drawCard, card)
+
     }
 }
