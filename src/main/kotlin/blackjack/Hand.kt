bbd2ec5d81971e025a7fd2122c0bea8a2e5643fc
knopgm <knop.gm@gmail.com>
2025-07-12 22:34:31 +0200

    feat (Hand): create method to sum card values and a test for it

--- src/main/kotlin/blackjack/Hand.kt
+++ src/main/kotlin/blackjack/Hand.kt
@@ -6,4 +6,9 @@ class Hand () {
     fun addCard(card: Card) {
         cards.add(card)
     }
+
+    fun sumCards(): Int {
+        val cardValues = cards.map{ card -> card.number }
+        return cardValues.sum()
+    }
 }
