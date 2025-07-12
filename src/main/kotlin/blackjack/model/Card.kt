ac047e7f8c75effd73c0b1a6292b7b9811a6c1f1
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-12 12:03:07 +0200

    refactor(Card): add method showCardFace()

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -4,4 +4,8 @@ class Card(val rank: Rank, val suit: Suit) {
     override fun toString(): String {
         return "${rank.name} of ${suit.name}"
     }
+
+    fun showCardFace():String{
+        return "${rank.face}${suit.symbol}"
+    }
 }
