f9489ebdaae3e0e21ee133728e6e5e2730c0cd41
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 00:38:29 +0200

    test: add a new test for InputView, Parsing process

--- src/test/kotlin/blackjack/view/InputViewTest.kt
+++ src/test/kotlin/blackjack/view/InputViewTest.kt
@@ -1,11 +1,28 @@
 package blackjack.view
 
-import org.junit.jupiter.api.Assertions.*
+import blackjack.view.InputView.Parser
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
     @Test
     fun `name should be parsed correctly`() {
+        val input = "mina, guri, ella"
+        val parsed = InputView.Parser.playerNames(input)
+        val expected = listOf("mina", "guri", "ella")
 
+        assertEquals(parsed, expected)
+    }
+
+    @ValueSource(strings = ["#####", "%23!", "m1112@", "111133/22", "#####, %23!", "#####, %23!"])
+    @ParameterizedTest
+    fun `throw if name format is not correct`(input: String) {
+
+        assertThrows<IllegalArgumentException> {
+            Parser.playerNames(input)
+        }
     }
 }
