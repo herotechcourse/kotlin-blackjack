7b0fee85c36625ed0c278b236366d59228e34dbc
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-13 15:19:10 +0200

    feat: add Blackjack state

---
+++ src/main/kotlin/blackjack/states/Blackjack.kt
@@ -0,0 +1,10 @@
+package blackjack.states
+
+import blackjack.model.Card
+import blackjack.model.Hand
+
+class Blackjack(override val hand: Hand) : State {
+    override fun draw(card: Card): State {
+        TODO("Not yet implemented")
+    }
+}
