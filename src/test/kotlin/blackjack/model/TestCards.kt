736b9c98ca6b6e77d369cc6559c59c34e68ab406
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-13 14:21:34 +0200

    test: add file with cards for tests

---
+++ src/test/kotlin/blackjack/model/TestCards.kt
@@ -0,0 +1,9 @@
+package blackjack.model
+
+object TestCards {
+    val Ace = Card.of(Suit.DIAMONDS, Rank.ACE)
+    val TWO = Card.of(Suit.DIAMONDS, Rank.TWO)
+    val QUEEN = Card.of(Suit.DIAMONDS, Rank.QUEEN)
+    val FOUR = Card.of(Suit.DIAMONDS, Rank.FOUR)
+    val JACK = Card.of(Suit.DIAMONDS, Rank.JACK)
+}
