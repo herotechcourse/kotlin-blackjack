2b0da4117a4115b69d8f24874ffc1b30202b2d48
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 02:02:29 +0200

    feat(player): implement evaluateState logic in Player
    - if player card points is 21 in the first round, blackjack
    - if over, bust
    - else -> controlled my GameManager with user input

--- src/main/kotlin/blackjack/model/participant/Player.kt
+++ src/main/kotlin/blackjack/model/participant/Player.kt
@@ -1,3 +1,27 @@
 package blackjack.model.participant
 
-class Player(name: String) : Participant(name)
+import blackjack.model.GameConstants.BLACKJACK_SCORE
+import blackjack.model.GameConstants.PLAYER_FIRST_HIT_COUNT
+import blackjack.model.state.State
+
+class Player(name: String) : Participant(name) {
+    override var _state: State = State.HIT
+        get() = evaluateState()
+
+    private fun evaluateState(): State {
+        if (isFirstRound()) return blackOrHit()
+        if (points > BLACKJACK_SCORE) return State.BUST
+        return _state
+    }
+
+    private fun isFirstRound(): Boolean {
+        return cardsCount() == PLAYER_FIRST_HIT_COUNT
+    }
+
+    private fun blackOrHit(): State {
+        return when (points) {
+            BLACKJACK_SCORE -> State.BLACKJACK
+            else -> State.HIT
+        }
+    }
+}
