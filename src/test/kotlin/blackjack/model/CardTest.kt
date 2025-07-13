887a1cb01f2bf6d5fcb1828c26a964952e760834
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-13 10:51:05 +0200

    refactor: implement pattern Object Pool for Card

---
+++ src/test/kotlin/blackjack/model/CardTest.kt
@@ -0,0 +1,18 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import kotlin.test.Test
+
+class CardTest {
+    @Test
+    fun `should return same instance for same card`() {
+        val suit = Suit.HEART
+        val rank = Rank.ACE
+
+        val card1 = Card.of(suit, rank)
+        val card2 = Card.of(suit, rank)
+
+        assertThat(card1).isSameAs(card2)
+        assertThat(card1).isEqualTo(card2)
+    }
+}
