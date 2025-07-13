9e2d43bd64bf2d7365a0df2837da53b55452bf15
knopgm <knop.gm@gmail.com>
2025-07-14 00:01:10 +0200

    refactor (BlackjackController): connect input and output view classes to the game controller

--- src/main/kotlin/blackjack/BlackjackController.kt
+++ src/main/kotlin/blackjack/BlackjackController.kt
@@ -2,21 +2,36 @@ package blackjack
 
 object BlackjackController {
     fun run() {
+//        val c1 = Card(CardSuit.CLUB, 2)
+//        val c2 = Card(CardSuit.HEART, 11)
+//        val hand = Hand()
+//        hand.addCard(c1)
+//        hand.addCard(c2)
+//        println(hand.toString())
+        // __________________________
+
         val game = GameLogic()
         val deck = Deck.generate()
         val dealer = Dealer(deck)
+        val input = InputView()
+        val output = OutputView()
 
-        val playersNames = listOf("Lili", "Jon")
+
+        val playersNames = input.readPlayersName()
         val players = game.namesToPlayers(playersNames)
+        output.printPlayersIntroToTheirCards(players)
         game.firstTurnCards(players, dealer)
-        // display first hand of each participant
-
-        // check hand status
+        output.printFirstTurnCards(dealer)
+        players.forEach { player ->
+            output.printFirstTurnCards(player)
+//            val playerStatus = player.hand.checkWinOrLose()
+        }
 
         game.otherTurnCards(players, dealer)
-        // display second hand of each participant
 
-        // check hand status
+//         display second hand of each participant
+
+//         check hand status
 
 
 //        if (gameBlackjackCheck == true) {
