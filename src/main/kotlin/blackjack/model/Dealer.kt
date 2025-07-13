d4e3cf15222a529f8146f456dfd8258ce0a6811e
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-13 22:27:26 +0200

    refactor(Dealer): use companion object

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -1,15 +1,9 @@
 package blackjack.model
 
-class Dealer(
-    override val name: String = "Dealer",
-    override var isActive: Boolean = true,
-    override val cardsInHand: MutableList<Card> = mutableListOf(),
-) : Participant {
-    fun mustDraw(totalValueOfCards: Int) = totalValueOfCards <= 16
+class Dealer : Participant("Dealer") {
+    fun mustDraw(): Boolean = total() <= DEALER_DRAW_LIMIT
 
-    fun getFinalResultForDealer(players: List<Player>): String {
-        val lose = players.count { it.isActive }
-        val win = players.count { !it.isActive }
-        return "Dealer: $win Win $lose Lose"
+    companion object {
+        const val DEALER_DRAW_LIMIT = 16
     }
 }
