aa525c1caaecbededb1de56a57e6a744a41d030a
knopgm <knop.gm@gmail.com>
2025-07-13 23:03:16 +0200

    feat (Card): create a method to convert card numbers into a card-face element and override the method toString to print a card as a string

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -9,4 +9,20 @@ class Card(val suit: CardSuit, val number: Int) {
         }
         return listOf(number)
     }
+
+    fun numberToCardDeckElements(number: Int): String {
+        return when (number) {
+            1 -> "A"
+            10 -> "J"
+            11 -> "Q"
+            12 -> "K"
+            else -> number.toString()
+        }
+    }
+
+    override fun toString(): String {
+        val cardValue = numberToCardDeckElements(number)
+        return "%s%s".format(cardValue, suit.value)
+    }
+
 }
