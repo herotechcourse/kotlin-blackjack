88e0f214dc8f6a96bd4dfcbe03ed30d2289fdac0
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-13 15:53:29 +0200

    test(Hit): add test, calling stay returns Stay state

--- src/test/kotlin/blackjack/states/HitTest.kt
+++ src/test/kotlin/blackjack/states/HitTest.kt
@@ -13,4 +13,20 @@ class HitTest {
 
         assertTrue(next is Hit)
     }
+
+    @Test
+    fun `drawing a card over 21 leads to Bust state`() {
+        val initial = Hit(Hand(listOf(TestCards.QUEEN, TestCards.JACK))) // 20
+        val next = initial.draw(TestCards.FOUR) // 24 → Bust
+
+        assertTrue(next is Bust)
+    }
+
+    @Test
+    fun `calling stay returns Stay state`() {
+        val initial = Hit(Hand(listOf(TestCards.QUEEN, TestCards.JACK)))
+        val next = initial.stay()
+
+        assertTrue(next is Stay)
+    }
 }
