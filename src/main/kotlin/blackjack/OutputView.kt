2cb83a676bcba75a3981f281d58b79197d0c99c5
knopgm <knop.gm@gmail.com>
2025-07-13 23:59:24 +0200

    feat (OutputView): create methods to print current cards of players after each hit

--- src/main/kotlin/blackjack/OutputView.kt
+++ src/main/kotlin/blackjack/OutputView.kt
@@ -16,6 +16,15 @@ class OutputView {
         println(DISPLAY_HANDS.format(participant.name, cards))
     }
 
+    fun printCurrentCardsOfOnePlayer (player: Player) {
+        val cards = player.hand.toString()
+        println(DISPLAY_HANDS.format(player.name, cards))
+    }
+    fun printCurrentDealerCards (dealer: Dealer) {
+        val cards = dealer.hand.toString()
+        println(DISPLAY_HANDS.format(dealer.name, cards))
+    }
+
     companion object Messages {
         const val ASK_NAMES = "Enter the names of the players (comma-separated):"
         const val FIRST_TURN_CARDS = "Dealing two cards to dealer, %s."
