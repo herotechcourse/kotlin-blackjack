d777ee3e87ef35ce9620ad75dcb92d4839ba1ce6
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-13 19:05:11 +0200

    refactor: change to data class

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -1,6 +1,6 @@
 package blackjack.model
 
-class Card(val rank: Rank, val suit: Suit) {
+data class Card(val rank: Rank, val suit: Suit) {
     override fun toString(): String {
         return "${rank.name} of ${suit.name}"
     }
