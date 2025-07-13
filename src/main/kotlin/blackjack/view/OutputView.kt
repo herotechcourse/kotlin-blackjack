850b89086602bc682aff56c6a6399fd525498669
Mina Kim <mina.kim@deliveryhero.com>
2025-07-13 04:15:13 +0200

    feat: Add a method that prints the final results

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -3,6 +3,8 @@ package blackjack.view
 import blackjack.model.participant.Dealer
 import blackjack.model.participant.Participant
 import blackjack.model.participant.Participants
+import blackjack.model.result.GameResult
+import blackjack.model.result.Outcome
 
 import blackjack.model.PlayerBackup
 import blackjack.model.Statistics
@@ -17,7 +19,7 @@ object OutputView {
     }
 
     fun showDealerDraw(dealer: Dealer) {
-        println("Dealer draws ${dealer.cardsCount() - 1} more card due to having 16 or less.")
+        println("\nDealer draws ${dealer.cardsCount() - 1} more card due to having 16 or less.\n")
     }
 
     fun printAskForCard(player: PlayerBackup) {
@@ -55,8 +57,35 @@ object OutputView {
         println("Error: $msg")
     }
 
+    fun showGameResult(gameResult: GameResult) {
+        val playersResults = gameResult.playersResults
+        showCards(gameResult.dealer, showPoints(gameResult.dealer))
+        playersResults.forEach { (player, _) ->
+            showCards(player, showPoints(player))
+        }
+
+        println("\n${Message.FINAL_RESULTS_TITLE}")
+
+        val dealerWin = playersResults.filter { it.value == Outcome.LOSE}.size
+        val dealerLose = playersResults.filter { it.value == Outcome.WIN }.size
+        val dealerDraw = playersResults.size - dealerLose - dealerWin
+        val showDraw = if (dealerDraw > 0) ", $dealerDraw Draw" else ""
+
+        println("Dealer: $dealerWin Win $dealerLose Lose$showDraw")
+
+        playersResults.forEach { (player, outcome) ->
+            println("${player.name}: ${outcome.name}")
+        }
+    }
+
+    private fun showPoints(participant: Participant): String {
+        return " - Total: ${participant.points}"
+    }
+
+
     object Message {
         const val ENTER_PLAYERS_NAMES = "Enter the names of the players (comma-separated):"
         const val GENERATE_NEW_CARD = " Card deck is empty... prepare for a new card deck."
+        const val FINAL_RESULTS_TITLE = "## Final Results"
     }
 }
