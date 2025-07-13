3271aba635599bc55df35652e6143b593e93beed
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-13 20:14:54 +0200

    test(CardTest): create tests with enum classes and check toString method

--- src/test/kotlin/blackjack/model/CardTest.kt
+++ src/test/kotlin/blackjack/model/CardTest.kt
@@ -7,7 +7,20 @@ class CardTest {
     @Test
     fun `should create a card with given rank and suit`() {
         val card = Card(Rank.TWO, Suit.HEARTS)
-        assertThat(card.rank.toString()).isEqualTo("TWO")
-        assertThat(card.suit.toString()).isEqualTo("HEARTS")
+        assertThat(card.rank).isEqualTo(Rank.TWO)
+        assertThat(card.suit).isEqualTo(Suit.HEARTS)
+    }
+
+    @Test
+    fun `toString should return correct card format`() {
+        val card = Card(Rank.KING, Suit.SPADES)
+        assertThat(card.toString()).isEqualTo("KING of SPADES")
+    }
+
+    @Test
+    fun `cards with same rank and suit should be equal`() {
+        val card1 = Card(Rank.ACE, Suit.DIAMONDS)
+        val card2 = Card(Rank.ACE, Suit.DIAMONDS)
+        assertThat(card1).isEqualTo(card2)
     }
 }
