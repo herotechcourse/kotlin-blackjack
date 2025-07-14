1fde50b5d0bb0fbd2d34478d366ce14efc4a39b1
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 11:15:55 +0200

    feat: implement super class Finished as a final state

---
+++ src/main/kotlin/blackjack/states/Finished.kt
@@ -0,0 +1,14 @@
+package blackjack.states
+
+import blackjack.model.Card
+import blackjack.view.Errors
+
+interface Finished : State {
+    override fun draw(card: Card): State {
+        throw IllegalStateException(Errors.INVALID_DRAW.message)
+    }
+
+    override fun stay(): State {
+        throw IllegalStateException(Errors.INVALID_STAY.message)
+    }
+}
