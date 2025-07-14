de9601b8a7209f52bcd3056ffaa2b6612cad0439
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-14 17:31:55 +0200

    test(game): update test code regarding to refactoring

--- src/test/kotlin/model/BlackJackTest.kt
+++ src/test/kotlin/model/BlackJackTest.kt
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test
 class BlackJackTest {
     @Test
     fun `deal 2 cards for each player at the start of the game`() {
-        val dealer = Dealer()
-        val players = listOf("A", "B", "C", "D", "E", "F").map { Player(it) }
-        BlackJack.initGame(players, dealer)
+        val game = BlackJack(listOf("A", "B", "C", "D", "E", "F"))
+        val players = game.players
+        val dealer = game.dealer
         Assertions.assertTrue { players.all { it.getCardsNumber() == 2 } }
         Assertions.assertTrue { dealer.getCardsNumber() == 2 }
     }
