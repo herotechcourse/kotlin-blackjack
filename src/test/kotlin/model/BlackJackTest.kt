71fe1d71ddf9bf72993626a6b647fe6f009f6a4b
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-14 11:16:43 +0200

    test(game): fix typo in class name

--- src/test/kotlin/model/BlackJackTest.kt
+++ src/test/kotlin/model/BlackJackTest.kt
@@ -8,7 +8,7 @@ class BlackJackTest {
     fun `deal 2 cards for each player at the start of the game`() {
         val dealer = Dealer()
         val players = listOf("A", "B", "C", "D", "E", "F").map { Player(it) }
-        BackJack.initGame(players, dealer)
+        BlackJack.initGame(players, dealer)
         Assertions.assertTrue { players.all { it.getCardsNumber() == 2 } }
         Assertions.assertTrue { dealer.getCardsNumber() == 2 }
     }
