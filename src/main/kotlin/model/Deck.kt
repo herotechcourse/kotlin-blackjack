bb32163206d436214492aac53a15449b6e1559b1
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-14 11:17:20 +0200

    style(deck): delete comment

--- src/main/kotlin/model/Deck.kt
+++ src/main/kotlin/model/Deck.kt
@@ -3,12 +3,10 @@ package model
 class Deck {
     private val cards = generateCards()
 
-    // for testing
     fun getCards(): Set<Card> {
         return cards.toSet()
     }
 
-    // TODO: Catch the IllegalArgumentException
     fun pop(): Card {
         require(cards.isNotEmpty()) { "The deck is empty" }
         return cards.first().also { cards.remove(it) }
