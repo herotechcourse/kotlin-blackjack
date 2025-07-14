20dd674c90a18f7fd43c10d97fb5d52ace6b8fcc
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 14:49:54 +0200

    feat: output dealer first card

---
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -0,0 +1,27 @@
+package blackjack.view
+
+import blackjack.model.Card
+import blackjack.model.Dealer
+import blackjack.model.Player
+
+object OutputView {
+    fun displayTableSetUp(
+        dealer: Dealer,
+        players: List<Player>,
+    ) {
+        displayDealerFirstCard(dealer)
+        displayPlayers(players)
+    }
+
+    fun displayPlayers(players: List<Player>) {
+        players.forEach { println("${it.name}'s cards: " + cardsText(it.state.hand.cards)) }
+    }
+
+    fun displayDealerFirstCard(dealer: Dealer) {
+        println("${dealer.name}'s cards: " + dealer.state.hand.cards[0].rank.face + dealer.state.hand.cards[0].suit.symbol)
+    }
+
+    private fun cardsText(cards: List<Card>): String {
+        return cards.joinToString(", ") { it.rank.face + it.suit.symbol }
+    }
+}
