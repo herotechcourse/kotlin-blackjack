ab9ced79638edb7d134fe225e1196c5da50d476e
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-14 17:34:05 +0200

    refactor(ResultCalculation): add methods to simplify calculating earnings

--- src/main/kotlin/blackjack/model/ResultCalculation.kt
+++ src/main/kotlin/blackjack/model/ResultCalculation.kt
@@ -2,10 +2,16 @@ package blackjack.model
 
 object ResultCalculation {
 
+    private fun hasDealerBJ(dealer: Dealer) = dealer.cardsInHand.hasBlackJack()
+
+    private fun hasPlayerBJ(player: Player) = player.cardsInHand.hasBlackJack()
+
+    private fun hasAnyPlayerBJ(players: Players) = players.getPlayers().any { player -> player.cardsInHand.hasBlackJack() }
+
     fun calculatePlayerEarnings(player: Player, dealer: Dealer): Int {
         return when {
-            dealer.cardsInHand.hasBlackJack() && player.cardsInHand.hasBlackJack() -> 0
-            player.cardsInHand.hasBlackJack() -> (player.bet * BlackJackValues.BET_MULTIPLICATOR).toInt()
+            hasDealerBJ(dealer) && hasPlayerBJ(player) -> 0
+            hasPlayerBJ(player) -> (player.bet * BlackJackValues.BET_MULTIPLICATOR).toInt()
             player.isPlaying && !dealer.isPlaying -> player.bet
             !player.isPlaying -> -(player.bet)
             else -> 0
@@ -14,11 +20,14 @@ object ResultCalculation {
 
     fun calculateDealerEarnings(dealer: Dealer, players: Players): Int {
         val playersEarnings = players.calculateTotalPlayersEarning()
+        var playersLoses = 0
+        if (playersEarnings < 0)
+            playersLoses = -(playersEarnings)
         return when {
-            dealer.cardsInHand.hasBlackJack() && players.getPlayers().any { player -> player.cardsInHand.hasBlackJack() } -> 0
-            dealer.cardsInHand.hasBlackJack() -> - playersEarnings
-            dealer.isPlaying && playersEarnings < 0 -> - playersEarnings
-            !dealer.isPlaying -> - playersEarnings
+            hasDealerBJ(dealer) && hasAnyPlayerBJ(players) -> 0
+            hasDealerBJ(dealer) -> playersLoses
+            dealer.isPlaying -> playersLoses
+            !dealer.isPlaying -> -playersEarnings
             else -> 0
         }
     }
