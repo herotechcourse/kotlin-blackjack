81654036b2f13583dc408f2a41eceb203ffff78b
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 19:05:57 +0200

    test: add test for calculating profit in Stay

--- src/main/kotlin/blackjack/states/Stay.kt
+++ src/main/kotlin/blackjack/states/Stay.kt
@@ -2,4 +2,26 @@ package blackjack.states
 
 import blackjack.model.Hand
 
-class Stay(override val hand: Hand) : Finished
+class Stay(override val hand: Hand) : Finished {
+    override fun profit(
+        state: State,
+        betMoney: Int,
+    ): Double {
+        return if (state.isBlackjack) {
+            betMoney * BLACKJACK_LOSE
+        } else if (state.hand.calculatePoints() == hand.calculatePoints()) {
+            betMoney * DRAW_TIE
+        } else if (state.hand.calculatePoints() > hand.calculatePoints()) {
+            betMoney * LOSE_LOSS
+        } else {
+            betMoney * WIN_GAIN
+        }
+    }
+
+    companion object {
+        private const val WIN_GAIN = 1.0
+        private const val LOSE_LOSS = -1.0
+        private const val DRAW_TIE = 0.0
+        private const val BLACKJACK_LOSE = -1.5
+    }
+}
