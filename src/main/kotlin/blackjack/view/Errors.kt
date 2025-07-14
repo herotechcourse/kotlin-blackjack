657dcf63aff8046476eb9cb27e2a74ae68206988
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 19:29:17 +0200

    refactor: Errors messages

--- src/main/kotlin/blackjack/view/Errors.kt
+++ src/main/kotlin/blackjack/view/Errors.kt
@@ -6,4 +6,6 @@ enum class Errors(val message: String) {
     INVALID_CARD("Invalid Card"),
     INVALID_DRAW("Invalid draw Command"),
     INVALID_STAY("Invalid stay Command"),
+    INVALID_BETTING_AMOUNT("Invalid Betting Amount"),
+    INVALID_PROFIT("Invalid Profit Exception"),
 }
