5b5a38434d85c501b0544694c37c61ddfb39c42e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 16:09:18 +0200

    feat: implement draw and stay for Player

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -2,4 +2,12 @@ package blackjack.model
 
 import blackjack.states.FirstTurn
 
-class Player(name: String) : Participant(name, FirstTurn(Hand(emptyList())))
+class Player(name: String) : Participant(name, FirstTurn(Hand(emptyList()))) {
+    fun draw(card: Card) {
+        state = state.draw(card)
+    }
+
+    fun stay() {
+        state = state.stay()
+    }
+}
