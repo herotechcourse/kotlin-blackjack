3eb03355a56a6e7189017a192be51c5755ecfff7
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 19:38:51 +0200

    feat: add setBettingAmount to player

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -3,11 +3,17 @@ package blackjack.model
 import blackjack.states.FirstTurn
 
 class Player(name: String) : Participant(name, FirstTurn(Hand(emptyList()))) {
+    private var bettingAmount: Int = 0
+
     fun draw(card: Card) {
         state = state.draw(card)
     }
 
     fun stay() {
         state = state.stay()
+    }
+
+    fun setBettingAmount(bettingAmount: Int) {
+        this.bettingAmount = bettingAmount
     }
 }
