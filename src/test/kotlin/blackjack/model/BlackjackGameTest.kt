b60545e3e88d9c7b747a5307add64d71cc303c57
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 17:18:48 +0200

    refactor: adapt test

--- src/test/kotlin/blackjack/model/BlackjackGameTest.kt
+++ src/test/kotlin/blackjack/model/BlackjackGameTest.kt
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test
 class BlackjackGameTest {
     @Test
     fun `after setUp table any player and dealer has 2 cards`() {
-        val cardDeck = CardDeck()
         val players = ParticipantsFactory.generatePlayers(listOf("Player 1", "Player 2"))
         val dealer = ParticipantsFactory.generateDealer()
 
-        BlackjackGame.setUpTable(dealer, players, cardDeck)
+        BlackjackGame(dealer, players, { true }, {})
 
         players.forEach { player ->
             assertThat(player.state.hand.size).isEqualTo(2)
