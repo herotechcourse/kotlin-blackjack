b8a42daac08442030330d2253e1b5819041e7442
farhanaahmed <farhana.ahmed.fima@gmail.com>
2025-07-14 16:11:00 +0200

    feat(Dealer): add method to calculate correct winning money for Dealer

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -3,6 +3,10 @@ package blackjack.model
 class Dealer : Participant("Dealer") {
     fun mustDraw(): Boolean = total() <= DEALER_DRAW_LIMIT
 
+    fun returnWinningMoneyForDealer(players: List<Player>): Int {
+        return players.sumOf { -it.returnWinningMoneyForPlayer() }
+    }
+
     companion object {
         const val DEALER_DRAW_LIMIT = 16
     }
