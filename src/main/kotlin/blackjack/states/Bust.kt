075c537f3ee50a5b215aa0dded90d2a070639bef
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 19:29:44 +0200

    refactor: override profit for bust

--- src/main/kotlin/blackjack/states/Bust.kt
+++ src/main/kotlin/blackjack/states/Bust.kt
@@ -2,4 +2,15 @@ package blackjack.states
 
 import blackjack.model.Hand
 
-class Bust(override val hand: Hand) : Finished
+class Bust(override val hand: Hand) : Finished {
+    override fun profit(
+        state: State,
+        betMoney: Int,
+    ): Double {
+        return betMoney * LOSE_LOSS
+    }
+
+    companion object {
+        private const val LOSE_LOSS = -1.0
+    }
+}
