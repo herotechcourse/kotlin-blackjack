789dda775967ff3b5bc8dcbae427be8250d8d836
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-14 14:35:47 +0200

    refactor(Player): modify condition in hasLessPointsThanDealer

--- src/main/kotlin/blackjack/model/Player.kt
+++ src/main/kotlin/blackjack/model/Player.kt
@@ -6,7 +6,7 @@ class Player(
     var bet: Int = 0
         private set
 
-    fun hasLessPointsThanDealer(dealerPoints: Int) = cardsInHand.calculateTotalValueOfCards() <= dealerPoints
+    fun hasLessPointsThanDealer(dealerPoints: Int) = cardsInHand.calculateTotalValueOfCards() < dealerPoints
 
     fun setBet(bet: Int) {
         this.bet = bet
