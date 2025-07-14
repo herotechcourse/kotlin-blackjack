8e9bead7372902e04542bad7ede5b60126e303a9
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-14 16:07:52 +0200

    refactor: make BlackjackGame as class, implement play function

--- src/main/kotlin/blackjack/model/BlackjackGame.kt
+++ src/main/kotlin/blackjack/model/BlackjackGame.kt
@@ -1,18 +1,35 @@
 package blackjack.model
 
-class BlackjackGame {
-    companion object {
-        fun setUpTable(
-            dealer: Dealer,
-            players: List<Player>,
-            cardDeck: CardDeck,
-        ) {
-            players.forEach { player ->
-                repeat(2) {
-                    player.state = player.state.draw(cardDeck.drawCard())
-                }
+import blackjack.states.Running
+
+class BlackjackGame(
+    private val dealer: Dealer,
+    private val players: List<Player>,
+    private val askForCard: (Player) -> Boolean,
+    private val output: (Player) -> Unit,
+) {
+    val cardDeck = CardDeck()
+
+    init {
+        setUpTable()
+    }
+
+    fun setUpTable() {
+        players.forEach { player ->
+            repeat(2) {
+                player.state = player.state.draw(cardDeck.drawCard())
             }
-            repeat(2) { dealer.state = dealer.state.draw(cardDeck.drawCard()) }
+        }
+        repeat(2) { dealer.state = dealer.state.draw(cardDeck.drawCard()) }
+    }
+
+    fun play() {
+        dealer.playTurn(cardDeck)
+        players.forEach {
+            while (it.state is Running && askForCard(it)) {
+                it.draw(cardDeck.drawCard())
+                output(it)
+            }
         }
     }
 }
