b6ab0558e68fd80eb551793aaf85af19cb344d14
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-14 16:37:27 +0200

    refactor(test, view): remove unnecessary `fromSymbol` and `toSymbol` functions from `SuitView` and `RankView`

--- src/test/kotlin/blackjack/model/CardTest.kt
+++ src/test/kotlin/blackjack/model/CardTest.kt
@@ -1,21 +1,39 @@
 package blackjack.model
 
-import blackjack.view.RankView
-import blackjack.view.SuitView
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class CardTest {
-    @ParameterizedTest(name = "Card - create valid digit value from {0}")
-    @ValueSource(strings = ["A♥", "3♣", "4♠", "5♥", "6♣", "7♥", "8♠", "9♣", "10♥", "J♦", "Q♣", "K♦", "3♥"])
-    fun `Card create valid digit from name`(candidate: String) {
-        val digit = candidate.dropLast(1)
-        val suitSymbol = candidate.last()
-        val rank = RankView.fromSymbol(digit)
-        val suit = SuitView.fromSymbol(suitSymbol)
-        val card = Card(rank, suit)
+    companion object {
+        @JvmStatic
+        fun cardProvider() =
+            listOf(
+                Arguments.of(Rank.ACE, Suit.HEARTS, "A♥"),
+                Arguments.of(Rank.THREE, Suit.CLUBS, "3♣"),
+                Arguments.of(Rank.FOUR, Suit.SPADES, "4♠"),
+                Arguments.of(Rank.FIVE, Suit.HEARTS, "5♥"),
+                Arguments.of(Rank.SIX, Suit.CLUBS, "6♣"),
+                Arguments.of(Rank.SEVEN, Suit.HEARTS, "7♥"),
+                Arguments.of(Rank.EIGHT, Suit.SPADES, "8♠"),
+                Arguments.of(Rank.NINE, Suit.CLUBS, "9♣"),
+                Arguments.of(Rank.TEN, Suit.HEARTS, "10♥"),
+                Arguments.of(Rank.JACK, Suit.DIAMONDS, "J♦"),
+                Arguments.of(Rank.QUEEN, Suit.CLUBS, "Q♣"),
+                Arguments.of(Rank.KING, Suit.DIAMONDS, "K♦"),
+                Arguments.of(Rank.THREE, Suit.HEARTS, "3♥"),
+            )
+    }
 
-        assertEquals(digit, RankView.toSymbol(card.rank))
+    @ParameterizedTest(name = "Card - create valid card {2} from rank {0} and suit {1}")
+    @MethodSource("cardProvider")
+    fun `Card creates valid string representation`(
+        rank: Rank,
+        suit: Suit,
+        expected: String,
+    ) {
+        val card = Card(rank, suit)
+        assertEquals(expected, card.toString())
     }
 }
