6524369d284c3f4a38b152bd6b3c2160eaeddb16
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-14 14:11:44 +0200

    refactor: Complete coverage for Gambler.kt.

--- src/test/kotlin/blackjack/GamblerTest.kt
+++ src/test/kotlin/blackjack/GamblerTest.kt
@@ -1,10 +1,6 @@
 package blackjack
 
-import blackjack.model.Card
-import blackjack.model.Gambler
-import blackjack.model.GamblerInfo
-import blackjack.model.Rank
-import blackjack.model.Suit
+import blackjack.model.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -38,5 +34,37 @@ class GamblerTest {
         val gambler = Gambler(GamblerInfo("Player"))
         gambler.addCard(listOf(Card(Rank.ACE, Suit.SPADE)))
         assertThat(gambler.hasCardCount()).isFalse
+    }
+
+    @Test
+    fun `return negative winnings if lost`() {
+        val gambler = Gambler(GamblerInfo("Player"))
+        gambler.setBetAmount(10.0)
+        gambler.calculateAndSetWinnings(false)
+        assertThat(gambler.winnings).isEqualTo(-10.0)
+    }
+
+    @Test
+    fun `return 1,5x winnings if BlackJack and Win`() {
+        val gambler = Gambler(GamblerInfo("Player"))
+        gambler.setBetAmount(10.0)
+        gambler.addCard(listOf(
+            Card(Rank.ACE, Suit.SPADE), Card(Rank.KING, Suit.SPADE))
+        )
+        gambler.calculateAndSetWinnings(true)
+
+        assertThat(gambler.winnings).isEqualTo(15.0)
+    }
+
+    @Test
+    fun `return 1x winnings if Win`() {
+        val gambler = Gambler(GamblerInfo("Player"))
+        gambler.setBetAmount(10.0)
+        gambler.addCard(listOf(
+            Card(Rank.ACE, Suit.SPADE), Card(Rank.THREE, Suit.SPADE))
+        )
+        gambler.calculateAndSetWinnings(true)
+
+        assertThat(gambler.winnings).isEqualTo(10.0)
     }
 }
