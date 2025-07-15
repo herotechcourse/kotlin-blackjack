f88ac746a99861f55fef2cc59bc6c2e32c7cd6ed
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-07-15 20:54:28 +0200

    feat: add subtraction support and update `Chips.zero` implementation
    - Added `minus` operator to `Chips` for subtraction functionality.
    - Updated `zero` from a function to a property for improved usage consistency.

--- src/main/kotlin/blackjack/model/Chips.kt
+++ src/main/kotlin/blackjack/model/Chips.kt
@@ -12,12 +12,12 @@ value class Chips(val value: Double) {
 
     operator fun plus(other: Chips): Chips = Chips(value + other.value)
 
-    override fun toString(): String = "${value.toInt()}"
+    operator fun minus(other: Chips): Chips = Chips(value - other.value)
 
     companion object {
         private const val BLACKJACK_MULTIPLIER = 1.5
         private const val ZERO = 0.0
-
-        fun zero(): Chips = Chips(ZERO)
+        val zero: Chips
+            get() = Chips(ZERO)
     }
 }
