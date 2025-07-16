32bfb7940060642cc58e569e3917750268b408fd
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-16 19:53:29 +0200

    refactor: compare the enum itself not the value

--- src/main/kotlin/blackjack/model/Hand.kt
+++ src/main/kotlin/blackjack/model/Hand.kt
@@ -6,7 +6,7 @@ class Hand(val cards: List<Card>) {
     operator fun plus(card: Card): Hand = Hand(this.cards + card)
 
     fun calculatePoints(): Int {
-        val countOfAce = cards.filter { it.rank.value == 1 }.size
+        val countOfAce = cards.filter { it.rank == Rank.ACE }.size
         var sum =
             cards.sumOf {
                 it.rank.value
