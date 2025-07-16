847e68a9d6d29eba680243f0ffa59bbf12ab7b53
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-16 19:33:02 +0200

    refactor: make number constant

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -5,11 +5,15 @@ import blackjack.states.Running
 
 class Dealer() : Participant("Dealer", FirstTurn(Hand(emptyList()))) {
     fun playTurn(deck: CardDeck) {
-        while (state is Running && state.hand.calculatePoints() < 17) {
+        while (state is Running && state.hand.calculatePoints() < SEVENTEEN) {
             state = state.draw(deck.drawCard())
         }
         if (state is Running) {
             state = state.stay()
         }
+    }
+
+    companion object {
+        const val SEVENTEEN = 17
     }
 }
