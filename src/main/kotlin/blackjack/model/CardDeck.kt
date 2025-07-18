0736a669afc6f40b30435c420cce84fc59dc4fe1
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-18 13:40:51 +0200

    refactor: specify type of variables

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -1,10 +1,10 @@
 package blackjack.model
 
 data class CardDeck(private val hold: MutableList<Card> = initPokerCards()) {
-    val size
+    val size: Int
         get() = cards.size
 
-    val cards
+    val cards: List<Card>
         get() = hold.toList()
 
     fun drawCard(): Card {
