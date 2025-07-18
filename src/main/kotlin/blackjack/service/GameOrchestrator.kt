90a4e0b581bde216ec3a9ffad29d880e23251c74
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-18 13:53:24 +0200

    refactor: separate controller's method to different classes

---
+++ src/main/kotlin/blackjack/service/GameOrchestrator.kt
@@ -0,0 +1,49 @@
+package blackjack.service
+
+import blackjack.model.Deck
+import blackjack.model.Player
+import blackjack.view.OutputView
+import blackjack.controller.Controller.Companion.BLACKJACK_SCORE
+
+class GameOrchestrator(private val deck: Deck, private val inputProcessor: InputProcessor) {
+    companion object {
+        private const val DEALER_THRESHOLD = 16
+        private const val INITIAL_CARDS_COUNT = 2
+    }
+
+    fun dealInitialCards(
+        dealer: Player,
+        players: List<Player>,
+    ) {
+        dealer.addCard(deck.drawCard(INITIAL_CARDS_COUNT))
+        players.forEach { it.addCard(deck.drawCard(INITIAL_CARDS_COUNT)) }
+    }
+
+    fun runPlayerTurn(player: Player) {
+        var wantsToHit = false
+        while (player.score <= BLACKJACK_SCORE) {
+            wantsToHit = inputProcessor.processHitOrStay(player.name)
+            when {
+                !wantsToHit -> break
+                else -> {
+                    player.addCard(deck.drawCard())
+                    OutputView.displayCardsOfPlayers(player)
+                }
+            }
+        }
+        when {
+            !wantsToHit && player.cards.size == INITIAL_CARDS_COUNT -> {
+                OutputView.displayCardsOfPlayers(player)
+            }
+        }
+    }
+
+    fun runDealerTurn(dealer: Player) {
+        if (dealer.score <= DEALER_THRESHOLD) {
+            OutputView.displayDealersTurn()
+            while (dealer.score <= DEALER_THRESHOLD) {
+                dealer.addCard(deck.drawCard())
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/blackjack/service/InputProcessor.kt
@@ -0,0 +1,83 @@
+package blackjack.service
+
+import blackjack.view.InputView
+import blackjack.view.OutputView
+
+class InputProcessor {
+    companion object {
+        private const val MAX_ATTEMPTS = 5
+        private const val MAX_ATTEMPT_MESSAGE = "Too many attempts"
+        const val YES = "y"
+        const val NO = "n"
+    }
+
+    fun processPlayerNames(): List<String> {
+        repeat(MAX_ATTEMPTS) {
+            val result = attemptToGetPlayerNames()
+            when {
+                result.isSuccess -> return result.getOrNull()!!
+                else -> OutputView.displayErrorMessages(result.exceptionOrNull()?.message)
+            }
+        }
+        throw IllegalArgumentException(MAX_ATTEMPT_MESSAGE)
+    }
+
+    fun processBetAmount(name: String): Int {
+        repeat(MAX_ATTEMPTS) {
+            val result = attemptToGetBetAmount(name)
+            when {
+                result.isSuccess -> return result.getOrNull()!!
+                else -> OutputView.displayErrorMessages(result.exceptionOrNull()?.message)
+            }
+        }
+        throw IllegalArgumentException(MAX_ATTEMPT_MESSAGE)
+    }
+
+    fun processHitOrStay(playerName: String): Boolean {
+        repeat(MAX_ATTEMPTS) {
+            val result = attemptToGetHitOrStay(playerName)
+            when {
+                result.isSuccess -> return result.getOrNull()!!
+                else -> OutputView.displayErrorMessages(result.exceptionOrNull()?.message)
+            }
+        }
+        throw IllegalArgumentException(MAX_ATTEMPT_MESSAGE)
+    }
+
+    private fun attemptToGetPlayerNames(): Result<List<String>> {
+        return runCatching {
+            val names = InputView.getNamesOfPlayers()
+            val validNames =
+                names
+                    .split(",")
+                    .map(String::trim)
+                    .filter { it.isNotBlank() }
+            when {
+                validNames.isEmpty() -> throw IllegalArgumentException("Player names cannot be empty")
+                else -> validNames
+            }
+        }
+    }
+
+    private fun attemptToGetBetAmount(name: String): Result<Int> {
+        return runCatching {
+            InputView.getBetAmount(name)
+        }
+    }
+
+    private fun attemptToGetHitOrStay(playerName: String): Result<Boolean> {
+        return runCatching {
+            val answer = InputView.getHitOrStand(playerName)
+            convertAnswerToBoolean(answer)
+        }
+    }
+
+    private fun convertAnswerToBoolean(answer: String): Boolean {
+        return when (answer) {
+            YES -> true
+            NO -> false
+            else -> throw IllegalArgumentException("The answer must be y or n.")
+        }
+    }
+
+}
