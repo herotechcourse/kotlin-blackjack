77a6bbae51f682b632e0a18d53f7c81d2633985d
Mina Kim <mina.kim@deliveryhero.com>
2025-07-20 00:15:55 +0200

    fix(game-result): fix condition to correctly assign Blackjack outcome

--- src/main/kotlin/blackjack/model/result/GameResult.kt
+++ src/main/kotlin/blackjack/model/result/GameResult.kt
@@ -17,7 +17,7 @@ class GameResult(val participants: Participants) {
     private fun getPlayerResult(player: Player): Outcome {
         return when {
             player.isBust() -> Outcome.LOSE
-            player.isBlackjack() && !dealer.isBust() -> Outcome.BLACKJACK
+            player.isBlackjack() && !dealer.isBlackjack() -> Outcome.BLACKJACK
             dealer.isBust() -> Outcome.WIN
             else -> compareWithDealer(player)
         }
