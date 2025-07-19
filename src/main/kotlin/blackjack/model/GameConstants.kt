e192484530b8d524f885467bf31e039f6f88b7ba
Mina Kim <mina.kim@deliveryhero.com>
2025-07-19 21:18:40 +0200

    refactor: rename Constants value to improve readability

--- src/main/kotlin/blackjack/model/GameConstants.kt
+++ src/main/kotlin/blackjack/model/GameConstants.kt
@@ -4,5 +4,5 @@ object GameConstants {
     const val DEALER_MUST_HIT_LIMIT = 16
     const val BLACKJACK_SCORE = 21
     const val DEALER_FIRST_HIT_COUNT = 1
-    const val PLAYER_FIRST_HIT_COUNT = 2
+    const val FIRST_ROUND_HIT_COUNTS = 2
 }
