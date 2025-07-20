d351483eb6b099152c53016c992c74b7ec34f729
knopgm <knop.gm@gmail.com>
2025-07-20 17:35:50 +0200

    test (DealerTest): implement test to draw the correct card

--- src/test/kotlin/blackjack/DealerTest.kt
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -4,9 +4,26 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class DealerTest {
+
+    @Test
+    fun `draws the correct card` () {
+        val cards = listOf(
+            Card( CardSuit.SPADE, 10),
+            Card(CardSuit.CLUB, 5),
+            Card(CardSuit.HEART, 2)
+        )
+        val deck = Deck(cards.toMutableList())
+        val dealer = Dealer(deck)
+
+        val drawCard = dealer.drawCard()
+        assertEquals(CardSuit.SPADE, drawCard.suit)
+        assertEquals(10, drawCard.number)
+        assertEquals(2, deck.cards.size)
+    }
+
     @Test
     fun `draws a card`() {
-        val deck = Deck.generate()
+        val deck = Deck.generate(false)
         Dealer(deck).drawCard()
         val deckSize = deck.cards.size
 
