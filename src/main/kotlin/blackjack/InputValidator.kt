3355a26d538b3c61b02245e818551887ce3252c6
knopgm <knop.gm@gmail.com>
2025-07-20 16:31:47 +0200

    refactor (InputValidator): improve validation by the use of length to check the amount of characters in a name input

--- src/main/kotlin/blackjack/InputValidator.kt
+++ src/main/kotlin/blackjack/InputValidator.kt
@@ -4,7 +4,7 @@ class InputValidator {
     fun validateNames (input: List<String>) {
         input.forEach { name ->
             require(name.isNotBlank()) { "Input cannot be blank." }
-            require(name.chars().count() > 2) { "Name should have more than 2 characters." }
+            require(name.length > 2) { "Name should have more than 2 characters." }
         }
     }
 }
