bc379b3e487ff1e707d7813efd5b49b681e1b538
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-30 20:51:50 +0200

    test(player): add test for player

---
+++ src/test/kotlin/model/PlayerTest.kt
@@ -0,0 +1,31 @@
+package model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PlayerTest {
+    @Test
+    fun `player should draw two cards`() {
+        val deck = Deck()
+        val player = Player("pobi")
+        val prevHandSize = player.getCardsNumber()
+        player.turn(deck, { }, dummyDecision())
+        assertThat(player.getCardsNumber() - prevHandSize).isEqualTo(N)
+    }
+
+    companion object {
+        const val N = 2
+
+        fun dummyDecision(): (BasePlayer) -> Boolean {
+            var counter = N
+            return { player: BasePlayer ->
+                if (counter > 0) {
+                    counter--
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+    }
+}
