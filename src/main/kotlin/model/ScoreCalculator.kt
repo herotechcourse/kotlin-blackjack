f41fe9aff00e18f965bc1eb9ee5fd98b14e57682
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-30 20:54:15 +0200

    refactor(game): replace class key word with object key word in ScoreCalculator

--- src/main/kotlin/model/ScoreCalculator.kt
+++ src/main/kotlin/model/ScoreCalculator.kt
@@ -1,21 +1,19 @@
 package model
 
-class ScoreCalculator {
-    companion object {
-        fun getScore(cards: Set<Card>): Int {
-            var scoreTotal = scoreSum(cards)
-            var aceCounter = aceCount(cards)
-            while (scoreTotal > 21 && hasAce(aceCounter)) {
-                scoreTotal -= 10
-                aceCounter--
-            }
-            return scoreTotal
+object ScoreCalculator {
+    fun getScore(cards: Set<Card>): Int {
+        var scoreTotal = scoreSum(cards)
+        var aceCounter = aceCount(cards)
+        while (scoreTotal > 21 && hasAce(aceCounter)) {
+            scoreTotal -= 10
+            aceCounter--
         }
-
-        private fun hasAce(aceCounter: Int): Boolean = aceCounter != 0
-
-        private fun scoreSum(cards: Set<Card>) = cards.sumOf { card -> Rank.score(card.rank) }
-
-        private fun aceCount(cards: Set<Card>) = cards.count { it.rank == Rank.ACE }
+        return scoreTotal
     }
+
+    private fun hasAce(aceCounter: Int): Boolean = aceCounter != 0
+
+    private fun scoreSum(cards: Set<Card>) = cards.sumOf { card -> Rank.score(card.rank) }
+
+    private fun aceCount(cards: Set<Card>) = cards.count { it.rank == Rank.ACE }
 }
