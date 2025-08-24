af7c012885daf66bddd0da28486155e4515898fe
knopgm <knop.gm@gmail.com>
2025-08-24 11:49:04 +0200

    test (DealerTest): add tests that ensure the rule of a dealer hit

--- src/test/kotlin/blackjack/DealerTest.kt
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -29,4 +29,40 @@ class DealerTest {
 
         assertEquals(47, deckSize, "Should draw a card out of the deck")
     }
+
+    @Test
+    fun `given specific cards, returns the true boolean option`() {
+        val cards =
+            mutableListOf(
+                Card(CardSuit.SPADE, 10),
+                Card(CardSuit.CLUB, 3),
+                Card(CardSuit.HEART, 2),
+            )
+        val deck = Deck(cards.toMutableList())
+        val dealer = Dealer(deck)
+
+        dealer.addCard(Card(CardSuit.SPADE, 10))
+        dealer.addCard(Card(CardSuit.CLUB, 3))
+        dealer.addCard(Card(CardSuit.HEART, 2))
+
+        assertEquals(true, dealer.shouldHit())
+    }
+
+    @Test
+    fun `given specific cards, returns the false boolean option`() {
+        val cards =
+            listOf(
+                Card(CardSuit.SPADE, 10),
+                Card(CardSuit.CLUB, 8),
+                Card(CardSuit.HEART, 2),
+            )
+        val deck = Deck(cards.toMutableList())
+        val dealer = Dealer(deck)
+
+        dealer.addCard(Card(CardSuit.SPADE, 10))
+        dealer.addCard(Card(CardSuit.CLUB, 8))
+        dealer.addCard(Card(CardSuit.HEART, 2))
+
+        assertEquals(false, dealer.shouldHit())
+    }
 }
