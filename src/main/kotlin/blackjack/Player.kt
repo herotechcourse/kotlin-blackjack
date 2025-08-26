a65a84f2b291751364ae4c1c18256fb371a09e6f
knopgm <knop.gm@gmail.com>
2025-08-26 10:50:34 +0200

    refactor (cleaning): remove unused files from the project

--- src/main/kotlin/blackjack/Player.kt
+++
@@ -1,8 +0,0 @@
-package blackjack
-
-class Player(playerName: String) : Participant(playerName) {
-    override fun shouldHit(): Boolean {
-        val scoreHand = this.sumCards()
-        return scoreHand < 21
-    }
-}
