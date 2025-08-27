175456d8519ad2fabef88d737188e4747403bb78
knopgm <knop.gm@gmail.com>
2025-08-27 21:36:49 +0200

    test (DealerTest): fix dealer tests

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -3,6 +3,7 @@ package blackjack.model
 import blackjack.enum.CardSuit
 import blackjack.state.Running
 import blackjack.state.Started
+import blackjack.state.State
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test
 class DealerTest {
     private lateinit var deck: Deck
     private lateinit var dealer: Dealer
+    private lateinit var state: State
 
     @BeforeEach
     fun setUp() {
@@ -55,19 +57,19 @@ class DealerTest {
     fun `shouldDraw returns true if Running and sum is less than 17`() {
         val hand = Hand(mutableListOf(Card(CardSuit.HEART, 2), Card(CardSuit.CLUB, 3)))
         dealer.state = Running(hand, deck)
-        assertTrue(dealer.shouldDraw())
+        assertTrue(state.shouldDraw(17))
     }
 
     @Test
     fun `shouldDraw returns false if Running and sum is greater than or equal to 17`() {
         val hand = Hand(mutableListOf(Card(CardSuit.HEART, 10), Card(CardSuit.CLUB, 7)))
         dealer.state = Running(hand, deck)
-        assertFalse(dealer.shouldDraw())
+        assertFalse(state.shouldDraw(17))
     }
 
     @Test
     fun `shouldDraw returns false if not Running`() {
         dealer.state = Started(Hand(), deck)
-        assertFalse(dealer.shouldDraw())
+        assertFalse(state.shouldDraw(17))
     }
 }
